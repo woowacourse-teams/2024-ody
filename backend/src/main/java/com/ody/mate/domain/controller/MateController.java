22f3e547270927ddecf5585ada42957fb23390f5
mzeong <mjeong@sookmyung.ac.kr>
2024-07-18 17:02:50 +0900

    docs: 모임 참여 API 문서화 (#11)
    * docs: 모임 개설 API DTO 이름 변경
    
    * docs: 모임 참여 API 문서화
    
    * docs: 모임 개설, 모임 참여 API 상태 코드 변경

--- backend/src/main/java/com/ody/mate/domain/controller/MateController.java
+++ backend/src/main/java/com/ody/mate/domain/controller/MateController.java
@@ -1,34 +1,32 @@
-package com.ody.meeting.controller;
+package com.ody.mate.domain.controller;
 
+import com.ody.mate.domain.dto.MateSaveRequest;
 import com.ody.meeting.dto.MateResponse;
-import com.ody.meeting.dto.MeetingSaveRequest;
 import com.ody.meeting.dto.MeetingSaveResponse;
 import java.net.URI;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
-public class MeetingController implements MeetingControllerSwagger {
+public class MateController implements MateControllerSwagger {
 
     @Override
-    @PostMapping("/meetings")
+    @PostMapping("/mates")
     public ResponseEntity<MeetingSaveResponse> save(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String fcmToken,
-            @RequestBody MeetingSaveRequest meetingSaveRequest
+            MateSaveRequest mateSaveRequest
     ) {
-        return ResponseEntity.created(URI.create("/meetings"))
+        return ResponseEntity.created(URI.create("/mates"))
                 .body(new MeetingSaveResponse(1L, "우테코 16조", LocalDate.parse("2024-07-15"), LocalTime.parse("14:00"),
-                        "서울 송파구 올림픽로35다길 42", "37.515298", "127.103113", 1, List.of(new MateResponse("오디")), "초대코드"));
+                        "서울 송파구 올림픽로35다길 42", "37.515298", "127.103113", 2,
+                        List.of(new MateResponse("오디"), new MateResponse("제리")), "초대코드"));
     }
 }
--- backend/src/main/java/com/ody/mate/domain/controller/MateControllerSwagger.java
+++ backend/src/main/java/com/ody/mate/domain/controller/MateControllerSwagger.java
@@ -1,6 +1,6 @@
-package com.ody.meeting.controller;
+package com.ody.mate.domain.controller;
 
-import com.ody.meeting.dto.MeetingSaveRequest;
+import com.ody.mate.domain.dto.MateSaveRequest;
 import com.ody.meeting.dto.MeetingSaveResponse;
 import com.ody.swagger.annotation.ErrorCode400;
 import com.ody.swagger.annotation.ErrorCode401;
@@ -11,24 +11,30 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
-@Tag(name = "Meeting API")
-public interface MeetingControllerSwagger {
+@Tag(name = "Mate API")
+public interface MateControllerSwagger {
 
     @Operation(
-            summary = "모임 개설",
-            requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = MeetingSaveRequest.class))),
+            summary = "모임 참여",
+            requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = MateSaveRequest.class))),
             responses = {
                     @ApiResponse(
                             responseCode = "201",
-                            description = "모임 개설 성공",
+                            description = "모임 참여 성공",
                             content = @Content(schema = @Schema(implementation = MeetingSaveResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "유효하지 않은 초대코드",
+                            content = @Content(schema = @Schema(implementation = ProblemDetail.class))
                     )
             }
     )
     @ErrorCode400
     @ErrorCode401
     @ErrorCode500
-    ResponseEntity<MeetingSaveResponse> save(String fcmToken, MeetingSaveRequest meetingSaveRequest);
+    ResponseEntity<MeetingSaveResponse> save(String fcmToken, MateSaveRequest mateSaveRequest);
 }
