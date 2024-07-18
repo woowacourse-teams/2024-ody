578b3a066e5edb58c35888b2b7d328d3be552045
eun-byeol <joeunbyeol98@gmail.com>
2024-07-18 17:21:00 +0900

    docs: 로그 목록 API 추가

---
+++ backend/src/main/java/com/ody/notification/dto/response/NotiLogFindResponse.java
@@ -0,0 +1,16 @@
+package com.ody.notification.dto.response;
+
+import com.ody.notification.dto.request.NotificationSaveResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+public record NotiLogFindResponse(
+
+        @Schema(
+                description = "로그 목록",
+                example = "[{\"type\": \"ENTRY\",\n\"nickname\": \"조조\",\n\"createdAt\": \"2024-07-17 08:59:32\"}]"
+        )
+        List<NotificationSaveResponse> notiLog
+) {
+
+}
