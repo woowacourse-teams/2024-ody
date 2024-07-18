5836b9505a1c5f8a7b0700300c6fe99a5eae492e
Hyeon0208 <dmsgktn0208@gmail.com>
2024-07-18 17:42:30 +0900

    refactor: 회원 추가 API 매핑 URL 추가

--- backend/src/main/java/com/ody/member/controller/MemberController.java
+++ backend/src/main/java/com/ody/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController implements MemberControllerSwagger {
 
     @Override
+    @PostMapping("/members")
     public ResponseEntity<Void> save(@RequestHeader(HttpHeaders.AUTHORIZATION) String fcmToken) {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
