548fb204db0fb9e11ffd51a8e1f26c4d579307aa
haeum808 <haeumueah@gmail.com>
2024-07-18 17:25:42 +0900

    chore: MainActivity -> IntroActivity 클래스명 변경

---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.woowacourse.ody.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.woowacourse.ody.R
+import timber.log.Timber
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
