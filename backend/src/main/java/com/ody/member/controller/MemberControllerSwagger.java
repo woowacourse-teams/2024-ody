fcf7229f66587921d8f347a0e27578bb7fcdaa45
Hyeon0208 <dmsgktn0208@gmail.com>
2024-07-18 17:40:46 +0900

    style: 코드 컨벤션 적용

--- backend/src/main/java/com/ody/member/controller/MemberControllerSwagger.java
+++ backend/src/main/java/com/ody/member/controller/MemberControllerSwagger.java
@@ -1,14 +1,7 @@
 package com.ody.member.controller;
 
-import com.ody.meeting.dto.MeetingSaveRequest;
-import com.ody.meeting.dto.MeetingSaveResponse;
-import com.ody.swagger.annotation.ErrorCode400;
-import com.ody.swagger.annotation.ErrorCode401;
 import com.ody.swagger.annotation.ErrorCode500;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
