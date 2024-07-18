85fcd6d831e4a05db82d72206f12bf6d5e40812d
Hyeon0208 <dmsgktn0208@gmail.com>
2024-07-18 17:39:48 +0900

    docs: 초대 코드 유효성 검사 API 문서 추가

--- backend/src/main/java/com/ody/meeting/controller/MeetingController.java
+++ backend/src/main/java/com/ody/meeting/controller/MeetingController.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,5 +32,14 @@ public class MeetingController implements MeetingControllerSwagger {
         return ResponseEntity.created(URI.create("/meetings"))
                 .body(new MeetingSaveResponse(1L, "우테코 16조", LocalDate.parse("2024-07-15"), LocalTime.parse("14:00"),
                         "서울 송파구 올림픽로35다길 42", "37.515298", "127.103113", 1, List.of(new MateResponse("오디")), "초대코드"));
+    }
+
+    @Override
+    @GetMapping("/invite-codes/{inviteCode}/validate")
+    public ResponseEntity<Void> validateInviteCode(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String fcmToken,
+            @PathVariable String inviteCode
+    ) {
+        return ResponseEntity.ok().build();
     }
 }
