183d273263476e1bf8d459826e9fc1263c5f0924
kimhyemin <kimhm0728@naver.com>
2024-07-19 17:24:28 +0900

    feat: 주소 검색 다이얼로그 띄우는 기능 구현

---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/address/AddressSearchDialog.kt
@@ -0,0 +1,46 @@
+package com.woowacourse.ody.presentation.address
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.woowacourse.ody.databinding.DialogAddressSearchBinding
+
+class AddressSearchDialog : DialogFragment() {
+    private var _binding: DialogAddressSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DialogAddressSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeView()
+    }
+
+    private fun initializeView() {
+        binding.bgAddressSearch.setOnClickListener { dismiss() }
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+        )
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
