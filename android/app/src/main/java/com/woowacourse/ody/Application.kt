f01ad98d68457b2ec02ee1b06b86c94e4d00b728
haeum808 <haeumueah@gmail.com>
2024-07-21 14:50:52 +0900

    feat: OdyDebugTree 구현

--- android/app/src/main/java/com/woowacourse/ody/Application.kt
+++ android/app/src/main/java/com/woowacourse/ody/Application.kt
@@ -1,15 +1,19 @@
 package com.woowacourse.ody
 
 import android.app.Application
-import com.google.android.datatransport.BuildConfig
 import timber.log.Timber
 
-class OdyApplication: Application() {
+class OdyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if(BuildConfig.DEBUG){
-            Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(OdyDebugTree)
         }
     }
+}
+
+object OdyDebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement): String =
+        "${element.fileName}:${element.lineNumber}#${element.methodName}"
 }
