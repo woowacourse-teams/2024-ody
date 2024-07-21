b82f16f1253b1197a07103bb9654d247c461e237
aprilgom <aprilgom@users.noreply.github.com>
2024-07-21 19:46:23 +0900

    feat: 회원 추가 서비스 구현

---
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/MemberService.kt
@@ -0,0 +1,12 @@
+package com.woowacourse.ody.data.remote
+
+import retrofit2.http.POST
+
+interface MemberService {
+    @POST(MEMBER_URL)
+    suspend fun postMember()
+
+    companion object{
+        private const val MEMBER_URL = "/members"
+    }
+}
