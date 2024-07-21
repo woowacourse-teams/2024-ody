547f2b9bb77312a1b47d4982d8d5667c58182345
kimhm0728 <kimhm0728@naver.com>
2024-07-21 23:14:13 +0900

    feat: 주소 검색 화면 구현 (#47)
    * design: 약속 장소 입력 ui 구현
    
    * design: 주소 검색 웹뷰 ui 구현
    
    * config: 데이터 바인딩, 뷰 바인딩 의존성 추가
    
    * feat: 주소 검색 다이얼로그 띄우는 기능 구현
    
    * design: 다이얼로그 ui 수정
    
    * style: ktLint 적용
    
    * config: 데이터 바인딩, 뷰 바인딩 의존성 추가 방식 변경
    
    * refactor: 다이얼로그 설정 코드 scope function으로 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.ktlint)
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -34,6 +35,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
     }
 }
 
--- android/app/src/main/java/com/woowacourse/ody/OdyApplication.kt
+++ android/app/src/main/java/com/woowacourse/ody/OdyApplication.kt
@@ -4,11 +4,11 @@ import android.app.Application
 import com.google.android.datatransport.BuildConfig
 import timber.log.Timber
 
-class OdyApplication: Application() {
+class OdyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if(BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
