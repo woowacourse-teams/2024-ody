388f6bd3a313035c5f40ce343fb24213597c3551
aprilgom <aprilgom@gmail.com>
2024-07-22 15:28:47 +0900

    refactor: Service 경로 postfix 이름 PATH로 변경

--- android/app/src/main/java/com/woowacourse/ody/data/remote/MemberService.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/MemberService.kt
@@ -3,10 +3,10 @@ package com.woowacourse.ody.data.remote
 import retrofit2.http.POST
 
 interface MemberService {
-    @POST(MEMBER_URL)
+    @POST(MEMBER_PATH)
     suspend fun postMember()
 
     companion object {
-        private const val MEMBER_URL = "/members"
+        private const val MEMBER_PATH = "/members"
     }
 }
