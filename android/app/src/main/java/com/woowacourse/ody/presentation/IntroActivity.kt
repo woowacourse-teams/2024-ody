321d59b6bfdc4823d39e264d5dc8686e399bc9da
aprilgom <aprilgom@gmail.com>
2024-07-22 16:51:37 +0900

    design: 인트로 화면 xml 작성

--- android/app/src/main/java/com/woowacourse/ody/presentation/IntroActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/IntroActivity.kt
@@ -9,7 +9,7 @@ import com.woowacourse.ody.R
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 
     companion object {
