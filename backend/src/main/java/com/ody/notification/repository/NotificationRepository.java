2367320fe557b053cdbb6e6f31f59cb8b1fbcd81
coli-geonwoo <coli-geonwoo@gmail.com>
2024-07-22 20:26:47 +0900

    feat: NotificationRepository 구현

---
+++ backend/src/main/java/com/ody/notification/repository/NotificationRepository.java
@@ -0,0 +1,18 @@
+package com.ody.notification.repository;
+
+import com.ody.notification.domain.Notification;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query("""
+            select noti
+            from Notification noti
+            left join fetch Mate m on noti.mate = m
+            left join Meeting meet on m.meeting = meet
+            where meet.id = :meetingId
+            """)
+    List<Notification> findAllMeetingLogsById(Long meetingId);
+
+}
