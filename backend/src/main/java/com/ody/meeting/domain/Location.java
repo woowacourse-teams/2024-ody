ba91550437655844e13938d2bfb6562d6eb61dac
Hyeon0208 <dmsgktn0208@gmail.com>
2024-07-22 23:22:12 +0900

    feat: 모임 정보 입력 검증 로직 추가

--- backend/src/main/java/com/ody/meeting/domain/Location.java
+++ backend/src/main/java/com/ody/meeting/domain/Location.java
@@ -2,16 +2,17 @@ package com.ody.meeting.domain;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 public class Location {
+
+    private static final List<String> SUPPORT_REGION = List.of("서울", "경기", "인천");
 
     @NotNull
     private String address;
@@ -21,4 +22,19 @@ public class Location {
 
     @NotNull
     private String longitude;
+
+    public Location(String address, String latitude, String longitude) {
+        validateSupportRegion(address);
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    private void validateSupportRegion(String address) {
+        SUPPORT_REGION.stream()
+                .filter(address::startsWith)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("현재 지원되지 않는 지역입니다."));
+    }
 }
+
