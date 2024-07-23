4a6c0fa6c312bde8aeeaa1c106b7f2668cd5841f
eun-byeol <joeunbyeol98@gmail.com>
2024-07-23 15:27:34 +0900

    refactor: RouteClient calculateDuration 메서드 수
    Co-authored-by: coli-geonwoo <kkwoo001021@naver.com>

--- backend/src/main/java/com/ody/route/config/RouteConfig.java
+++ backend/src/main/java/com/ody/route/config/RouteConfig.java
@@ -1,15 +1,28 @@
 package com.ody.route.config;
 
+import com.ody.route.service.RouteClient;
 import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 
 @Configuration
+@EnableConfigurationProperties(RouteProperties.class)
+@RequiredArgsConstructor
 public class RouteConfig {
+
+    private final RouteProperties routeProperties;
+
+    @Bean
+    public RouteClient routeClient(RestClient.Builder routeRestClientBuilder) {
+        return new RouteClient(routeProperties, routeRestClientBuilder);
+    }
 
     @Bean
     public RestClientCustomizer routeRestClientCustomizer() {
--- backend/src/main/java/com/ody/route/service/RouteClient.java
+++ backend/src/main/java/com/ody/route/service/RouteClient.java
@@ -1,63 +1,49 @@
 package com.ody.route.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.ody.meeting.domain.Location;
+import com.ody.route.config.RouteProperties;
 import com.ody.route.domain.Duration;
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-@Component
 public class RouteClient {
 
-    private final String key;
-    private final String url;
+    private final RouteProperties routeProperties;
     private final RestClient restClient;
 
     public RouteClient(
-            @Value("${odsay.secret-key}") String key,
-            @Value("${odsay.url}") String url,
+            RouteProperties routeProperties,
             RestClient.Builder routeRestClientBuilder
     ) {
-        this.key = key;
-        this.url = url;
+        this.routeProperties = routeProperties;
         this.restClient = routeRestClientBuilder.build();
     }
 
     public Duration calculateDuration(Location origin, Location target) {
-        JsonNode response = restClient.get()
+        return restClient.get()
                 .uri(makeURI(origin, target))
                 .retrieve()
-                .body(JsonNode.class);
-        return parse(response);
+                .body(Duration.class);
     }
 
     private URI makeURI(Location origin, Location target) {
+        String originLongitude = "?SX=";
+        String originLatitude = "&SY=";
+        String targetLongitude = "&EX=";
+        String targetLatitude = "&EY=";
+        String apiKey = "&apiKey=";
+
         try {
-            return new URI(url
-                    + "?SX=" + origin.getLongitude()
-                    + "&SY=" + origin.getLatitude()
-                    + "&EX=" + target.getLongitude()
-                    + "&EY=" + target.getLatitude()
-                    + "&apiKey=" + key
+            return new URI(routeProperties.getUrl()
+                    + originLongitude + origin.getLongitude()
+                    + originLatitude + origin.getLatitude()
+                    + targetLongitude + target.getLongitude()
+                    + targetLatitude + target.getLatitude()
+                    + apiKey + routeProperties.getApiKey()
             );
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    Duration parse(JsonNode response) {
-        if (response.isNull()) {
-            throw new IllegalArgumentException("route response가 null 입니다.");
-        }
-        int minutes = response.get("result")
-                .get("path")
-                .get(0)
-                .get("info")
-                .get("totalTime")
-                .asInt();
-        return new Duration(minutes);
     }
 }
