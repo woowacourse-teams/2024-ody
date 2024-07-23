66102c2868ec2b33200d40c8b751516ae5d9ffba
aprilgom <aprilgom@users.noreply.github.com>
2024-07-23 16:11:54 +0900

    feat: 알림 권한 요청 (#75)
    * feat: 알림 권한 요청
    
    * refactor: 리뷰 반영
    
    * refactor: 리뷰 반영

--- android/app/src/main/java/com/woowacourse/ody/presentation/intro/IntroActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/intro/IntroActivity.kt
@@ -1,11 +1,17 @@
 package com.woowacourse.ody.presentation.intro
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.woowacourse.ody.R
 import com.woowacourse.ody.databinding.ActivityIntroBinding
 import com.woowacourse.ody.presentation.meetinginfo.MeetingInfoActivity
 import com.woowacourse.ody.util.observeEvent
@@ -15,11 +21,20 @@ class IntroActivity : AppCompatActivity() {
     private val binding: ActivityIntroBinding by lazy {
         ActivityIntroBinding.inflate(layoutInflater)
     }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (isGranted.not()) {
+                showSnackBar(getString(R.string.intro_notification_permission_required))
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeObserve()
         initializeBinding()
+        requestNotificationPermission()
     }
 
     private fun initializeBinding() {
@@ -44,8 +59,25 @@ class IntroActivity : AppCompatActivity() {
     }
 
     private fun navigateToInviteCodeActivity() {
-        Log.d("Hello", "초대 코드를 입력하는 액티비티로 이동")
     }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    showSnackBar(getString(R.string.intro_notification_permission_guide))
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        }
+    }
+
+    private fun showSnackBar(message: String) = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
 
     companion object {
         fun getIntent(context: Context): Intent = Intent(context, IntroActivity::class.java)
