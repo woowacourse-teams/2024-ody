e0c101c1ccb97590168dd2ba7165a37aea07784c
aprilgom <aprilgom@gmail.com>
2024-07-23 10:53:12 +0900

    feat: 각 버튼 클릭에 따른 Navigation Event 발생 구현

---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/intro/IntroViewModel.kt
@@ -0,0 +1,20 @@
+package com.woowacourse.ody.presentation.intro
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.woowacourse.ody.util.Event
+import com.woowacourse.ody.util.emit
+
+class IntroViewModel : ViewModel(), IntroListener {
+    private val _navigateAction: MutableLiveData<Event<IntroNavigateAction>> = MutableLiveData(null)
+    val navigateAction: LiveData<Event<IntroNavigateAction>> get() = _navigateAction
+
+    override fun onClickInputInviteCodeButton() {
+        _navigateAction.emit(IntroNavigateAction.NavigateToInviteCode)
+    }
+
+    override fun onClickMeetingInfoButton() {
+        _navigateAction.emit(IntroNavigateAction.NavigateToMeetingInfo)
+    }
+}
