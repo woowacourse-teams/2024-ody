9c3b9cc4776d0d951fa49cee2451f92c584931c8
haeum808 <haeumueah@gmail.com>
2024-07-23 10:53:41 +0900

    chore: ktlint apply true로 변경

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint) apply true
     alias(libs.plugins.googleServices) apply false
 }
