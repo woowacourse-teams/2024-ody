e59ac9cb3a3def3247932338a6cdd854953c46f1
aprilgom <aprilgom@gmail.com>
2024-07-24 01:09:32 +0900

    refactor: DefaultNotificationLogRepository 싱글턴으로 변경

--- android/app/src/main/java/com/woowacourse/ody/data/remote/DefaultNotificationLogRepository.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/DefaultNotificationLogRepository.kt
@@ -4,8 +4,9 @@ import com.woowacourse.ody.data.model.toNotificationList
 import com.woowacourse.ody.domain.NotificationLog
 import com.woowacourse.ody.domain.NotificationLogRepository
 
-class DefaultNotificationLogRepository(
-    private val notificationService: NotificationService,
-) : NotificationLogRepository {
+object DefaultNotificationLogRepository : NotificationLogRepository {
+    private val notificationService: NotificationService =
+        RetrofitClient.retrofit.create(NotificationService::class.java)
+
     override suspend fun getNotificationLogs(): List<NotificationLog> = notificationService.getNotificationLogs().toNotificationList()
 }
