75b0ed41bfbcaadcf6f75689c4e6b3af2b1cab03
mzeong <mjeong@sookmyung.ac.kr>
2024-07-23 22:33:21 +0900

    feat: 전역 예외 처리기 추가

---
+++ backend/src/main/java/com/ody/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.ody.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = OdyException.class)
+    public ProblemDetail handleArgumentException(final OdyException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+}
