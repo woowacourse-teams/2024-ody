81c347b80220d9af7a649aa2fbfc6dfc5f4598d7
haeum808 <haeumueah@gmail.com>
2024-07-23 17:44:46 +0900

    feat: 날짜 선택 화면 구현

--- android/app/src/main/java/com/woowacourse/ody/presentation/date/MeetingDateFragment.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/date/MeetingDateFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.woowacourse.ody.R
 import com.woowacourse.ody.databinding.FragmentMeetingDateBinding
+import java.util.Calendar
 
 class MeetingDateFragment : Fragment() {
     private var _binding: FragmentMeetingDateBinding? = null
@@ -20,8 +23,39 @@ class MeetingDateFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeCalendar()
+    }
+
+    private fun initializeCalendar() {
+        val today = Calendar.getInstance().timeInMillis
+        binding.cvDate.minDate = today
+        binding.cvDate.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, dayOfMonth)
+
+            if (selectedDate.timeInMillis < today) {
+                showSnackBar(getString(R.string.meeting_date_date_guide))
+                binding.cvDate.date = today
+            } else {
+                // 추후에 ViewModel에서 관리
+                val selectedYear = year
+                val selectedMonth = month + 1
+                val selectedDay = dayOfMonth
+                showSnackBar("$selectedYear $selectedMonth $selectedDay")
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun showSnackBar(message: String) = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
 }
