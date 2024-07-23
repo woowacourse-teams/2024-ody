69abd7abe0089ae6b0761ac1b46fe55747de5fe5
kimhyemin <kimhm0728@naver.com>
2024-07-24 01:27:20 +0900

    refactor: NumberPicker 숫자 지정하는 기능을 프래그먼트로 이동

--- android/app/src/main/java/com/woowacourse/ody/presentation/meetinginfo/MeetingInfoViewModel.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/meetinginfo/MeetingInfoViewModel.kt
@@ -3,16 +3,5 @@ package com.woowacourse.ody.presentation.meetinginfo
 import androidx.lifecycle.ViewModel
 
 class MeetingInfoViewModel : ViewModel() {
-    val hours: List<String> by lazy { MEETING_HOUR_RANGE.toStrings() }
-    val minutes: List<String> by lazy { MEETING_MINUTE_RANGE.toStrings() }
 
-    private fun IntRange.toStrings() = map {
-        val string = it.toString()
-        if (string.length == 1) "0$string" else string
-    }
-
-    companion object {
-        private val MEETING_HOUR_RANGE = 0..23
-        private val MEETING_MINUTE_RANGE = 0..59
-    }
 }
--- android/app/src/main/java/com/woowacourse/ody/presentation/time/MeetingTimeFragment.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/time/MeetingTimeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.woowacourse.ody.databinding.FragmentMeetingTimeBinding
@@ -39,12 +40,30 @@ class MeetingTimeFragment : Fragment() {
     }
 
     private fun initializeView() {
-        binding.npMeetingTimeHour.wrapSelectorWheel = true
-        binding.npMeetingTimeMinute.wrapSelectorWheel = true
+        binding.npMeetingTimeHour.setRangeValues(MEETING_HOUR_RANGE)
+        binding.npMeetingTimeMinute.setRangeValues(MEETING_MINUTE_RANGE)
+    }
+
+    private fun NumberPicker.setRangeValues(values: IntRange) {
+        wrapSelectorWheel = true
+        val displayValues = values.toStrings()
+        minValue = 0
+        maxValue = displayValues.size - 1
+        displayedValues = displayValues.toTypedArray()
+    }
+
+    private fun IntRange.toStrings() = map {
+        val string = it.toString()
+        if (string.length == 1) "0$string" else string
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val MEETING_HOUR_RANGE = 0..23
+        private val MEETING_MINUTE_RANGE = 0..59
     }
 }
