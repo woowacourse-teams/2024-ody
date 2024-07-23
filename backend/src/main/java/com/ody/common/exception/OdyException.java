f22d4cd6d01e590e0a548a58cb2031e4709e7b17
mzeong <mjeong@sookmyung.ac.kr>
2024-07-23 19:33:49 +0900

    refactor: Nickname 도메인 개선
    - OdyException 커스텀 예외 생성
    - 이름 변경 (NickName -> Nickname)

---
+++ backend/src/main/java/com/ody/common/exception/OdyException.java
@@ -0,0 +1,8 @@
+package com.ody.common.exception;
+
+public class OdyException extends RuntimeException {
+
+    public OdyException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/com/ody/mate/domain/Nickname.java
+++ backend/src/main/java/com/ody/mate/domain/Nickname.java
@@ -1,5 +1,6 @@
 package com.ody.mate.domain;
 
+import com.ody.common.exception.OdyException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,22 +9,22 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class NickName {
+public class Nickname {
 
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 10;
 
-    private String nickName;
+    private String nickname;
 
-    public NickName(String nickName) {
-        validateLength(nickName);
-        this.nickName = nickName;
+    public Nickname(String nickname) {
+        validateLength(nickname);
+        this.nickname = nickname;
     }
 
-    private void validateLength(String nickName) {
-        if (nickName.length() < MIN_LENGTH || nickName.length() >= MAX_LENGTH) {
+    private void validateLength(String nickname) {
+        if (nickname.length() < MIN_LENGTH || nickname.length() >= MAX_LENGTH) {
             String message = String.format("닉네임은 %d글자 이상, %d자 미만으로 입력 가능합니다.", MIN_LENGTH, MAX_LENGTH);
-            throw new IllegalArgumentException(message);
+            throw new OdyException(message);
         }
     }
 }
