7e75fb284c54292e7f417015c0cbcb276d25ad65
aprilgom <aprilgom@gmail.com>
2024-07-23 14:49:40 +0900

    feat: 알림 권한 요청

--- android/app/src/main/java/com/woowacourse/ody/presentation/intro/IntroActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/intro/IntroActivity.kt
@@ -1,11 +1,17 @@
 package com.woowacourse.ody.presentation.intro
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.woowacourse.ody.R
 import com.woowacourse.ody.databinding.ActivityIntroBinding
 import com.woowacourse.ody.presentation.meetinginfo.MeetingInfoActivity
 import com.woowacourse.ody.util.observeEvent
@@ -20,6 +26,7 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         initializeObserve()
         initializeBinding()
+        requestNotificationPermission()
     }
 
     private fun initializeBinding() {
@@ -44,7 +51,39 @@ class IntroActivity : AppCompatActivity() {
     }
 
     private fun navigateToInviteCodeActivity() {
-        Log.d("Hello", "초대 코드를 입력하는 액티비티로 이동")
+    }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 권한 설정 안내 토스트 띄우기
+                    showToast(getString(R.string.intro_notification_permission_guide))
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+            }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (!isGranted) {
+                // 권한 요청 거부한 경우 안내 토스트 띄우기
+                showToast(getString(R.string.intro_notification_permission_required))
+            }
+        }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
