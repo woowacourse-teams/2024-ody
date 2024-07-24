687ed50e41f02aa2270ec1506da15e5aa544bc71
mzeong <mjeong@sookmyung.ac.kr>
2024-07-24 21:48:53 +0900

    feat: 모임 내 닉네임 중복 검증

--- backend/src/main/java/com/ody/mate/service/MateService.java
+++ backend/src/main/java/com/ody/mate/service/MateService.java
@@ -1,5 +1,6 @@
 package com.ody.mate.service;
 
+import com.ody.common.exception.OdyException;
 import com.ody.mate.domain.Mate;
 import com.ody.mate.dto.request.MateSaveRequest;
 import com.ody.mate.repository.MateRepository;
@@ -7,6 +8,7 @@ import com.ody.meeting.domain.Meeting;
 import com.ody.member.domain.Member;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +21,11 @@ public class MateService {
 
     @Transactional
     public Mate save(MateSaveRequest mateSaveRequest, Meeting meeting, Member member) {
-        return mateRepository.save(mateSaveRequest.toMate(meeting, member));
+        try {
+            return mateRepository.save(mateSaveRequest.toMate(meeting, member));
+        } catch (DataIntegrityViolationException exception) {
+            throw new OdyException("모임 내 닉네임이 중복되었습니다.");
+        }
     }
 
     public List<Mate> findAllByMeetingId(Long meetingId) {
