6cf6c1e58f11c59da63228f011885b4f32d90660
aprilgom <aprilgom@gmail.com>
2024-07-24 10:03:16 +0900

    design: item_notification_log.xml 작성

---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/notificationlog/BindingAdapter.kt
@@ -0,0 +1,17 @@
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.woowacourse.ody.R
+import com.woowacourse.ody.domain.NotificationType
+import com.woowacourse.ody.presentation.notificationlog.uimodel.NotificationLogUiModel
+
+@BindingAdapter("setNotificationLogText")
+fun TextView.setNotificationLogText(log: NotificationLogUiModel) {
+    this.text =
+        when (log.type) {
+            NotificationType.ENTRY -> this.context.getString(R.string.item_notification_entry, log.nickname)
+            NotificationType.DEPARTURE_REMINDER -> this.context.getString(R.string.item_notification_departure_reminder, log.nickname)
+            NotificationType.DEPARTURE -> this.context.getString(R.string.item_notification_departure, log.nickname)
+            NotificationType.DEFAULT -> ""
+        }
+}
