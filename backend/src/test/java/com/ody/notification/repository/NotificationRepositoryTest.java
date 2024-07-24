26b4789305cce766354b5ce3cf3d349b9b19f5b1
coli-geonwoo <coli-geonwoo@gmail.com>
2024-07-24 15:14:40 +0900

    refactor: auditing config 분리

--- backend/src/test/java/com/ody/notification/repository/NotificationRepositoryTest.java
+++ backend/src/test/java/com/ody/notification/repository/NotificationRepositoryTest.java
@@ -2,6 +2,7 @@ package com.ody.notification.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ody.common.config.JpaAuditingConfig;
 import com.ody.mate.domain.Mate;
 import com.ody.mate.domain.repository.MateRepository;
 import com.ody.meeting.domain.Location;
@@ -18,7 +19,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+@Import(JpaAuditingConfig.class)
 @DataJpaTest
 class NotificationRepositoryTest {
 
