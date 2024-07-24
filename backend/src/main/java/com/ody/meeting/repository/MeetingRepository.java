d3062f029bd972870813833e09d4226a8d0b0374
coli-geonwoo <coli-geonwoo@gmail.com>
2024-07-25 01:31:53 +0900

    feat: 멤버가 참여한 모임 리스트 반환 쿼리 구현

--- backend/src/main/java/com/ody/meeting/repository/MeetingRepository.java
+++ backend/src/main/java/com/ody/meeting/repository/MeetingRepository.java
@@ -1,8 +1,19 @@
 package com.ody.meeting.repository;
 
 import com.ody.meeting.domain.Meeting;
+import com.ody.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
+    @Query("""
+            select meet
+            from Meeting meet
+            right join Mate mate on mate.meeting.id = meet.id
+            where mate.member = :member
+            """
+    )
+    List<Meeting> findAllMeetingsByMember(Member member);
 }
