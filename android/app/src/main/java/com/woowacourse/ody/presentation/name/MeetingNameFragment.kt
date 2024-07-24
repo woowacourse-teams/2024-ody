7be55dc54b4827a2119b830345827678788aea6d
kimhyemin <kimhm0728@naver.com>
2024-07-24 13:58:14 +0900

    design: 모임 이름 입력 ui 구현

--- android/app/src/main/java/com/woowacourse/ody/presentation/name/MeetingNameFragment.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/name/MeetingNameFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.woowacourse.ody.databinding.FragmentMeetingNameBinding
+import com.woowacourse.ody.presentation.meetinginfo.MeetingInfoViewModel
 
 class MeetingNameFragment : Fragment() {
     private var _binding: FragmentMeetingNameBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MeetingInfoViewModel by activityViewModels<MeetingInfoViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +22,16 @@ class MeetingNameFragment : Fragment() {
     ): View {
         _binding = FragmentMeetingNameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeBinding()
+    }
+
+    private fun initializeBinding() {
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
     override fun onDestroyView() {
