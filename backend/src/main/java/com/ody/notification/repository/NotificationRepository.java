1437eac557e16ded71b57c598a0dfa5709c424d7
H <99153215+hyeon0208@users.noreply.github.com>
2024-07-25 21:38:48 +0900

    feat: 로그 목록 조회 시 과거순 정렬 (#144)
    * refactor: 아규먼트 리졸버 어노테이션 적용
    
    * refactor: 데이터베이스에 데이터 추가 순으로 알림 로그 정렬
    
    * test: 모임 반환 테스트 오류 해결
    
    * refactor: 현재 이전의 로그 목록을 조회하도록 수정
    
    * refactor: NotiLog 바인딩 수정
    
    createdAt > sendAt
    
    * refactor: 불필요한 레코드 삭제
    
    * refactor: LocalTime 밀리 세컨즈 제거
    
    * test: 로그 필터링 적용 테스트 작성
    
    ---------
    
    Co-authored-by: coli-geonwoo <coli-geonwoo@gmail.com>

--- backend/src/main/java/com/ody/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/ody/notification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.ody.notification.repository;
 
 import com.ody.notification.domain.Notification;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +13,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             from Notification noti
             left join fetch Mate m on noti.mate = m
             left join Meeting meet on m.meeting = meet
-            where meet.id = :meetingId
+            where meet.id = :meetingId and noti.sendAt <= now()
+            order by noti.sendAt asc
             """)
     List<Notification> findAllMeetingLogs(Long meetingId);
 }
