e17acaa7ca38916d7b39b2c7d5d8875679e81a86
mzeong <mjeong@sookmyung.ac.kr>
2024-07-25 15:11:25 +0900

    fix: ODsay 사용하는 MeetingControllerTest 코드 Disabled 처리

--- backend/src/test/java/com/ody/meeting/controller/MeetingControllerTest.java
+++ backend/src/test/java/com/ody/meeting/controller/MeetingControllerTest.java
@@ -17,12 +17,14 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
+@Disabled
 class MeetingControllerTest extends BaseControllerTest {
 
     @Autowired
