c922c5ba55ab203cf1d543201988705282806f04
haeum808 <haeumueah@gmail.com>
2024-07-25 09:27:48 +0900

    feat: MeetingRepository에 postMeeting 메서드 추가

--- android/app/src/main/java/com/woowacourse/ody/data/local/fake/FakeMeetingRepository.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/local/fake/FakeMeetingRepository.kt
@@ -1,5 +1,7 @@
 package com.woowacourse.ody.data.local.fake
 
+import com.woowacourse.ody.data.model.meeting.MeetingRequest
+import com.woowacourse.ody.data.model.meeting.MeetingResponse
 import com.woowacourse.ody.domain.Mate
 import com.woowacourse.ody.domain.Meeting
 import com.woowacourse.ody.domain.repository.MeetingRepository
@@ -16,11 +18,28 @@ object FakeMeetingRepository : MeetingRepository {
             0,
             "meetingA",
             "선릉 캠퍼스",
-            LocalDateTime.of(2024, 1, 1, 10, 0, 0),
+            "2024-01-01",
+            "10:00",
             mates,
         )
 
     override suspend fun fetchInviteCodeValidity(inviteCode: String): Result<Unit> = Result.success(Unit)
 
     override suspend fun fetchMeeting(): Result<List<Meeting>> = Result.success(listOf(meetingA))
+
+    override suspend fun postMeeting(meetingRequest: MeetingRequest): Result<MeetingResponse> =
+        Result.success(
+            MeetingResponse(
+                0,
+                "meetingA",
+                "선릉 캠퍼스",
+                LocalDateTime.of(2024, 1, 1, 10, 0, 0).toString(),
+                "A,B,C",
+                "A,B,C",
+                "A,B,C",
+                1,
+                listOf(),
+                "",
+            ),
+        )
 }
--- android/app/src/main/java/com/woowacourse/ody/domain/repository/MeetingRepository.kt
+++ android/app/src/main/java/com/woowacourse/ody/domain/repository/MeetingRepository.kt
@@ -1,9 +1,13 @@
 package com.woowacourse.ody.domain.repository
 
+import com.woowacourse.ody.data.model.meeting.MeetingRequest
+import com.woowacourse.ody.data.model.meeting.MeetingResponse
 import com.woowacourse.ody.domain.Meeting
 
 interface MeetingRepository {
     suspend fun fetchInviteCodeValidity(inviteCode: String): Result<Unit>
 
     suspend fun fetchMeeting(): Result<List<Meeting>>
+
+    suspend fun postMeeting(meetingRequest: MeetingRequest): Result<MeetingResponse>
 }
