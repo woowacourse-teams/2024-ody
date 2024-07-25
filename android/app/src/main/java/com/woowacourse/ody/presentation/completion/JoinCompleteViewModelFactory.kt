baa5ec244b7f762b97ff60809d1ff66034485635
haeum808 <haeumueah@gmail.com>
2024-07-25 09:28:22 +0900

    feat: meeting response 전달 및 초대 코드 저장 구현

---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/completion/JoinCompleteViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.woowacourse.ody.presentation.completion
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.woowacourse.ody.data.local.db.OdyDatastore
+import com.woowacourse.ody.domain.repository.MeetingRepository
+
+class JoinCompleteViewModelFactory(
+    private val meetingRepository: MeetingRepository,
+    private val datastore: OdyDatastore,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(JoinCompleteViewModel::class.java)) {
+            JoinCompleteViewModel(
+                meetingRepository,
+                datastore,
+            ) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
