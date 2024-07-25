a30bf2d2d7d5757dd71346206dcb0f603b617f99
aprilgom <aprilgom@gmail.com>
2024-07-25 13:35:53 +0900

    feat: 참여한 모임이 있을 경우 첫 번째 모임에 참여, 아닐 경우 인트로 화면으로 이동하는 기능 구형

--- android/app/src/main/java/com/woowacourse/ody/presentation/splash/SplashActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/splash/SplashActivity.kt
@@ -4,25 +4,43 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.woowacourse.ody.R
+import com.woowacourse.ody.data.remote.repository.DefaultMeetingRepository
 import com.woowacourse.ody.presentation.intro.IntroActivity
+import com.woowacourse.ody.presentation.notificationlog.NotificationLogActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    val viewModel: SplashViewModel by viewModels {
+        SplashViewModelFactory(DefaultMeetingRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         window.makeTransparentStatusBar()
+        viewModel.initialize()
+        initializeObserve()
+    }
 
-        lifecycleScope.launch {
-            delay(1500)
-            startActivity(IntroActivity.getIntent(this@SplashActivity))
-            finish()
+    private fun initializeObserve() {
+        viewModel.meeting.observe(this) {
+            lifecycleScope.launch {
+                delay(1500)
+                val intent =
+                    if (it == null) {
+                        IntroActivity.getIntent(this@SplashActivity)
+                    } else {
+                        NotificationLogActivity.getIntent(this@SplashActivity, it)
+                    }
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
---
+++ android/app/src/main/java/com/woowacourse/ody/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,29 @@
+package com.woowacourse.ody.presentation.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woowacourse.ody.domain.model.Meeting
+import com.woowacourse.ody.domain.repository.MeetingRepository
+import kotlinx.coroutines.launch
+
+class SplashViewModel(
+    private val meetingRepository: MeetingRepository,
+) : ViewModel() {
+    private val _meeting: MutableLiveData<Meeting?> = MutableLiveData()
+    val meeting: LiveData<Meeting?> get() = _meeting
+
+    private fun fetchMeeting() =
+        viewModelScope.launch {
+            meetingRepository.fetchMeeting().onSuccess {
+                _meeting.postValue(it.first())
+            }.onFailure {
+                _meeting.postValue(null)
+            }
+        }
+
+    fun initialize() {
+        fetchMeeting()
+    }
+}
