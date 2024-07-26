31e210271aec51d107fd980bd95464fced685e64
kimhyemin <kimhm0728@naver.com>
2024-07-26 11:44:26 +0900

    style: ktLint 적용

--- android/app/src/main/java/com/woowacourse/ody/presentation/splash/SplashActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/splash/SplashActivity.kt
@@ -12,7 +12,6 @@ import com.woowacourse.ody.R
 import com.woowacourse.ody.data.remote.repository.DefaultMeetingRepository
 import com.woowacourse.ody.presentation.intro.IntroActivity
 import com.woowacourse.ody.presentation.notificationlog.NotificationLogActivity
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
