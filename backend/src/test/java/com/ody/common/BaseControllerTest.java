5cb604fe83cb1254b8a55fb5635b40803af48915
eun-byeol <joeunbyeol98@gmail.com>
2024-07-27 21:56:14 +0900

    test: controller layer FakeRouteClient 적용, @Disabled 제거

--- backend/src/test/java/com/ody/common/BaseControllerTest.java
+++ backend/src/test/java/com/ody/common/BaseControllerTest.java
@@ -3,6 +3,7 @@ package com.ody.common;
 import com.ody.notification.config.FcmConfig;
 import com.ody.notification.service.FcmPushSender;
 import com.ody.notification.service.FcmSubscriber;
+import com.ody.route.service.TestRouteConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@Import(TestRouteConfig.class)
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class BaseControllerTest {
 
