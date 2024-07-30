e675f82e501cfdd457b5b1db1f61630627782318
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-07-30 09:04:13 -0700

    refactor: 매핑 메서드 Mapper.kt 에서 각 Model의 메서드로 이동 (#182)
    * refactor: 매핑 메서드 Mapper.kt 에서 각 Model의 메서드로 이동
    
    * refactor: 매핑 메서드들 각 Mapper로 이동

--- android/app/src/main/java/com/woowacourse/ody/data/remote/core/repository/DefaultNotificationLogRepository.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/core/repository/DefaultNotificationLogRepository.kt
@@ -1,7 +1,7 @@
 package com.woowacourse.ody.data.remote.core.repository
 
 import com.woowacourse.ody.data.remote.core.RetrofitClient
-import com.woowacourse.ody.data.remote.core.entity.toNotificationList
+import com.woowacourse.ody.data.remote.core.entity.notification.response.toNotificationList
 import com.woowacourse.ody.data.remote.core.service.NotificationService
 import com.woowacourse.ody.domain.model.NotificationLog
 import com.woowacourse.ody.domain.repository.ody.NotificationLogRepository
