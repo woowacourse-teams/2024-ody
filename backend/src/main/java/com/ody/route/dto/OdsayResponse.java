188d651c20d23418f760898635f818c706eca9a5
coli-geonwoo <148152234+coli-geonwoo@users.noreply.github.com>
2024-08-02 00:24:44 +0900

    refactor: RouteClient 응답 분기를 객체로 캡슐화 (#188)
    * refactor: RouteClient 응답 분기를 객체로 캡슐화
    
    * chore: 패키지 디렉토리 변경
    
    domain > dto
    
    * refactor : 응답 매핑 로직을 mapper로 이전
    
    * refactor : 불필요한 static 객체 삭제
    
    * refactor : 500에러 처리 로직 추가
    
    * refactor : NPE 예외 처리
    
    * refactor : 롬복으로 private 생성자 구현
    
    * test: OdsayResponseMapperTest 작성
    
    * style: message 개행 변경
    
    Co-authored-by: H <99153215+hyeon0208@users.noreply.github.com>
    
    * refactor: OdsayRouteClient에 mapper 로직 반영
    
    * fix: FutureOrPresentDateTimeValidatorTest 오류 개선
    
    ---------
    
    Co-authored-by: coli-geonwoo <coli-geonwoo@gmail.com>
    Co-authored-by: H <99153215+hyeon0208@users.noreply.github.com>

--- backend/src/main/java/com/ody/route/dto/OdsayResponse.java
+++ backend/src/main/java/com/ody/route/dto/OdsayResponse.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import java.util.OptionalLong;
 
 public record OdsayResponse(
-
         Optional<String> code,
         Optional<String> message,
         OptionalLong minutes
