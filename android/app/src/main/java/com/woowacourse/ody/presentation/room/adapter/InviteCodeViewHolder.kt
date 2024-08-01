6cfcd5259c691509e7265601788d73aea48f327f
aprilgom <aprilgom@users.noreply.github.com>
2024-08-02 00:24:04 +0900

    refactor: 이벤트 리스너 네이밍 수정

--- android/app/src/main/java/com/woowacourse/ody/presentation/room/adapter/InviteCodeViewHolder.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/room/adapter/InviteCodeViewHolder.kt
@@ -2,7 +2,7 @@ package com.woowacourse.ody.presentation.room.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.woowacourse.ody.databinding.ItemInviteCodeLogBinding
-import com.woowacourse.ody.presentation.room.CopyInviteCodeListener
+import com.woowacourse.ody.presentation.room.listener.CopyInviteCodeListener
 
 class InviteCodeViewHolder(private val binding: ItemInviteCodeLogBinding) :
     RecyclerView.ViewHolder(binding.root) {
