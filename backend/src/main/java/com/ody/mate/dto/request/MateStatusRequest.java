a98a52fac57cd2e6849f8caf1a31436646a1f604
Hyeon0208 <dmsgktn0208@gmail.com>
2024-08-03 10:36:46 +0900

    docs: 참여자 위치 상태 조회 요청 dto 위경도 변경
    Co-authored-by: coli-geonwoo <coli-geonwoo@gmail.com>

--- backend/src/main/java/com/ody/mate/dto/request/MateStatusRequest.java
+++ backend/src/main/java/com/ody/mate/dto/request/MateStatusRequest.java
@@ -8,11 +8,11 @@ public record MateStatusRequest(
         @Schema(description = "위치추적 불가 여부", example = "false")
         boolean isMissing,
 
-        @Schema(description = "현재 위도", example = "126.234524")
+        @Schema(description = "현재 위도", example = "39.123345")
         @Nullable
         String currentLatitude,
 
-        @Schema(description = "현재 경도", example = "39.123345")
+        @Schema(description = "현재 경도", example = "126.234524")
         @Nullable
         String currentLongitude
 ) {
