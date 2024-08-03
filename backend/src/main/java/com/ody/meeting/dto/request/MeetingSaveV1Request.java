0790c91f19c8a47fa3dd257598bbd893c37f29ea
coli-geonwoo <coli-geonwoo@gmail.com>
2024-08-03 18:31:07 +0900

    feat: 약속 개설 v1 api dto 구현

---
+++ backend/src/main/java/com/ody/meeting/dto/request/MeetingSaveV1Request.java
@@ -0,0 +1,36 @@
+package com.ody.meeting.dto.request;
+
+import com.ody.common.annotation.FutureOrPresentDateTime;
+import com.ody.mate.dto.request.MateSaveRequest;
+import com.ody.meeting.domain.Location;
+import com.ody.meeting.domain.Meeting;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@FutureOrPresentDateTime(dateFieldName = "date", timeFieldName = "time")
+public record MeetingSaveV1Request(
+
+        @Schema(description = "모임 이름", example = "우테코 16조")
+        @Size(min = 1, max = 15, message = "약속 이름은 1글자 이상, 16자 미만으로 입력 가능합니다.")
+        String name,
+
+        @Schema(description = "모임 날짜", type = "string", example = "2024-07-15")
+        LocalDate date,
+
+        @Schema(description = "모임 시간", type = "string", example = "14:00")
+        LocalTime time,
+
+        @Schema(description = "도착지 주소", example = "서울 송파구 올림픽로35다길 42")
+        String targetAddress,
+
+        @Schema(description = "도착지 위도", example = "37.515298")
+        String targetLatitude,
+
+        @Schema(description = "도착지 경도", example = "127.103113")
+        String targetLongitude
+) {
+
+}
+
