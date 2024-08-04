ab2ecfe7229542d4bfe11353c0cfc0e1119952b2
coli-geonwoo <coli-geonwoo@gmail.com>
2024-08-04 18:41:51 +0900

    refactor : 모임 > 약속 용어 통일

--- backend/src/main/java/com/ody/meeting/controller/MeetingControllerSwagger.java
+++ backend/src/main/java/com/ody/meeting/controller/MeetingControllerSwagger.java
@@ -12,6 +12,7 @@ import com.ody.member.domain.Member;
 import com.ody.notification.dto.response.NotiLogFindResponses;
 import com.ody.swagger.annotation.ErrorCode400;
 import com.ody.swagger.annotation.ErrorCode401;
+import com.ody.swagger.annotation.ErrorCode404;
 import com.ody.swagger.annotation.ErrorCode500;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -29,12 +30,12 @@ import org.springframework.http.ResponseEntity;
 public interface MeetingControllerSwagger {
 
     @Operation(
-            summary = "모임 개설",
+            summary = "약속 개설",
             requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = MeetingSaveRequest.class))),
             responses = {
                     @ApiResponse(
                             responseCode = "201",
-                            description = "모임 개설 성공",
+                            description = "약속 개설 성공",
                             content = @Content(schema = @Schema(implementation = MeetingSaveResponse.class))
                     )
             }
@@ -67,11 +68,11 @@ public interface MeetingControllerSwagger {
     ResponseEntity<MeetingWithMatesResponse> findMeetingWithMates(@Parameter(hidden = true) Member member, Long meetingId);
 
     @Operation(
-            summary = "참여중인 모임 목록 조회",
+            summary = "참여중인 약속 목록 조회",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "모임 목록 조회 성공",
+                            description = "약속 목록 조회 성공",
                             content = @Content(schema = @Schema(implementation = MeetingSaveResponses.class))
                     )
             }
@@ -90,7 +91,7 @@ public interface MeetingControllerSwagger {
                     ),
                     @ApiResponse(
                             responseCode = "404",
-                            description = "존재하지 않은 모임방이거나 모임방 일원이 아닌 경우",
+                            description = "존재하지 않은 약속방이거나 약속방 일원이 아닌 경우",
                             content = @Content(schema = @Schema(implementation = ProblemDetail.class))
                     )
             }
@@ -103,15 +104,11 @@ public interface MeetingControllerSwagger {
             summary = "초대코드 유효성 검사",
             responses = {
                     @ApiResponse(responseCode = "200", description = "유효한 초대 코드"),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "유효하지 않은 초대코드",
-                            content = @Content(schema = @Schema(implementation = ProblemDetail.class))
-                    )
             }
     )
     @ErrorCode400
     @ErrorCode401
+    @ErrorCode404(description = "유효하지 않은 초대코드")
     @ErrorCode500
     ResponseEntity<Void> validateInviteCode(@Parameter(hidden = true) Member member, String inviteCode);
 
@@ -147,15 +144,11 @@ public interface MeetingControllerSwagger {
                             responseCode = "400",
                             description = "클라이언트 입력 오류 또는 약속 시간 30분 전보다 이른 시간에 조회 시도 시",
                             content = @Content(schema = @Schema(implementation = ProblemDetail.class))
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "존재하지 않는 모임이거나 해당 모임 참여자가 아닌 경우",
-                            content = @Content(schema = @Schema(implementation = ProblemDetail.class))
                     )
             }
     )
     @ErrorCode401
+    @ErrorCode404(description = "존재하지 않는 약속이거나 해당 약속 참여자가 아닌 경우")
     @ErrorCode500
     ResponseEntity<MateEtaResponses> findAllMateEtas(
             @Parameter(hidden = true) Member member,
