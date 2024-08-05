abe108675f4bb3a2f4518928a2b3453957b94cf1
kimhyemin <kimhm0728@naver.com>
2024-08-05 17:45:21 +0900

    feat: 유저 위치 현황 Repository 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- android/app/src/main/java/com/woowacourse/ody/data/remote/core/repository/DefaultMeetingRepository.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/core/repository/DefaultMeetingRepository.kt
@@ -1,8 +1,11 @@
 package com.woowacourse.ody.data.remote.core.repository
 
-import com.woowacourse.ody.data.remote.core.entity.meeting.request.toMeetingRequest
-import com.woowacourse.ody.data.remote.core.entity.meeting.response.toMeeting
+import com.woowacourse.ody.data.remote.core.entity.meeting.mapper.toMateEtas
+import com.woowacourse.ody.data.remote.core.entity.meeting.request.MatesEtaRequest
+import com.woowacourse.ody.data.remote.core.entity.meeting.mapper.toMeetingRequest
+import com.woowacourse.ody.data.remote.core.entity.meeting.mapper.toMeeting
 import com.woowacourse.ody.data.remote.core.service.MeetingService
+import com.woowacourse.ody.domain.model.MateEta
 import com.woowacourse.ody.domain.model.Meeting
 import com.woowacourse.ody.domain.model.MeetingCreationInfo
 import com.woowacourse.ody.domain.repository.ody.MeetingRepository
@@ -12,8 +15,23 @@ class DefaultMeetingRepository(private val service: MeetingService) : MeetingRep
         return runCatching { service.getInviteCodeValidity(inviteCode) }
     }
 
-    override suspend fun fetchMeeting(): Result<List<Meeting>> = runCatching { service.getMeeting().meetings.map { it.toMeeting() } }
+    override suspend fun fetchMeeting(): Result<List<Meeting>> =
+        runCatching { service.getMeeting().meetings.map { it.toMeeting() } }
 
     override suspend fun postMeeting(meetingCreationInfo: MeetingCreationInfo): Result<String> =
         runCatching { service.postMeeting(meetingCreationInfo.toMeetingRequest()).inviteCode }
+
+    override suspend fun patchMatesEta(
+        meetingId: Long,
+        isMissing: Boolean,
+        currentLatitude: String,
+        currentLongitude: String
+    ): Result<List<MateEta>> {
+        return runCatching {
+            service.patchMatesEta(
+                meetingId,
+                MatesEtaRequest(isMissing, currentLatitude, currentLongitude),
+            ).toMateEtas()
+        }
+    }
 }
