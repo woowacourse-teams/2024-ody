fcbc0447837616949e9f080b6e30728830990453
H <99153215+hyeon0208@users.noreply.github.com>
2024-08-05 21:54:13 +0900

    feat: 약속 참여 V1 API 구현 (#247)
    * docs: 404에러 문서화를 위한 커스텀 어노테이션 추가
    
    * docs: Swagger 회원 참여 API 문서화
    
    * refactor: AliasFor 어노테이션으로 ApiResponse 속성값 매핑
    
    * refactor: api url 버전명 추가
    
    * feat: 기존의 /mates 요청 메서드 추가
    
    * feat: 약속 참여 v1 API 구현
    
    * style: 코드 컨벤션 적용
    
    * chore: mate 패키지로 이동
    
    * feat: 참여자 저장 응답 DTO 추가 및 지원하지 않는 메서드 제거
    
    * docs: 리스트 형식 데이터를 @ArraySchema 사용해 표시
    
    * refactor: 지원하지 않는 meeting 메서드 제거

--- backend/src/main/java/com/ody/mate/controller/MateController.java
+++ backend/src/main/java/com/ody/mate/controller/MateController.java
@@ -2,9 +2,14 @@ package com.ody.mate.controller;
 
 import com.ody.common.annotation.AuthMember;
 import com.ody.mate.dto.request.MateSaveRequest;
+import com.ody.mate.dto.response.MateResponse;
+import com.ody.mate.dto.response.MateSaveResponse;
+import com.ody.mate.service.MateService;
 import com.ody.meeting.dto.response.MeetingSaveResponse;
-import com.ody.meeting.service.MeetingService;
 import com.ody.member.domain.Member;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MateController implements MateControllerSwagger {
 
-    private final MeetingService meetingService;
+    private final MateService mateService;
 
     @PostMapping("/mates")
     public ResponseEntity<MeetingSaveResponse> save(
             @AuthMember Member member,
             @RequestBody MateSaveRequest mateSaveRequest
     ) {
-        MeetingSaveResponse meetingSaveResponse = meetingService.findAndSendNotifications(mateSaveRequest, member);
+        MeetingSaveResponse meetingSaveResponse = new MeetingSaveResponse(
+                1L,
+                "우테코 16조",
+                LocalDate.parse("2024-07-15"),
+                LocalTime.parse("14:00"),
+                "서울 송파구 올림픽로35다길 42",
+                "37.515298",
+                "127.103113",
+                1,
+                List.of(new MateResponse("오디")),
+                "초대코드"
+        );
+
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(meetingSaveResponse);
     }
 
     @Override
     @PostMapping("/v1/mates")
-    public ResponseEntity<MeetingSaveResponse> saveV1(
+    public ResponseEntity<MateSaveResponse> saveV1(
             @AuthMember Member member,
             @RequestBody MateSaveRequest mateSaveRequest
     ) {
-        MeetingSaveResponse meetingSaveResponse = meetingService.findAndSendNotifications(mateSaveRequest, member);
+        MateSaveResponse mateSaveResponse = mateService.saveAndSendNotifications(mateSaveRequest, member);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(meetingSaveResponse);
+                .body(mateSaveResponse);
     }
 }
