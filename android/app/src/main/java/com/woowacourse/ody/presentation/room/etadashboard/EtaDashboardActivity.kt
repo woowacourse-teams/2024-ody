586a79ffabdb989d8c44fe8ac4158ed0c65ba72e
kimhm0728 <kimhm0728@naver.com>
2024-08-06 14:38:06 +0900

    feat: 유저 현황 화면 리스트 어댑터 구현 (#265)
    * feat: Eta item에 대한 uiModel 구현
    
    * feat: adapter 구현 및 데이터 바인딩 연결
    
    * style: ktLint 적용
    
    * refactor: 함수 분리
    
    * feat: 위치 권한 팝업 띄우는 로직 구현
    
    * refactor: missing tooltip 리스너를 바인딩 어댑터로 분리
    
    * style: ktLint 적용
    
    * refactor: Pair 대신 Point 객체 사용
    
    * refactor: magic number 제거 및 DurationMinuteType 구현
    
    * style: ktLint 적용

--- android/app/src/main/java/com/woowacourse/ody/presentation/room/etadashboard/EtaDashboardActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/room/etadashboard/EtaDashboardActivity.kt
@@ -1,11 +1,71 @@
 package com.woowacourse.ody.presentation.room.etadashboard
 
+import android.graphics.Point
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.annotation.StringRes
 import com.woowacourse.ody.R
 import com.woowacourse.ody.databinding.ActivityEtaDashboardBinding
+import com.woowacourse.ody.databinding.LayoutMissingTooltipBinding
 import com.woowacourse.ody.presentation.common.binding.BindingActivity
+import com.woowacourse.ody.presentation.room.etadashboard.adapter.MateEtasAdapter
+import com.woowacourse.ody.presentation.room.etadashboard.listener.MissingToolTipListener
 
-class EtaDashboardActivity : BindingActivity<ActivityEtaDashboardBinding>(R.layout.activity_eta_dashboard) {
+class EtaDashboardActivity :
+    BindingActivity<ActivityEtaDashboardBinding>(R.layout.activity_eta_dashboard),
+    MissingToolTipListener {
+    private val adapter: MateEtasAdapter by lazy { MateEtasAdapter(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializeListAdapter()
+    }
+
+    private fun initializeListAdapter() {
+        binding.rvDashboard.adapter = adapter
+    }
+
     override fun initializeBinding() {
         // TODO
+    }
+
+    override fun onClickMissingToolTipListener(
+        point: Point,
+        isUserSelf: Boolean,
+    ) {
+        val messageId = if (isUserSelf) R.string.location_permission_self_guide else R.string.location_permission_friend_guide
+        showPopupWindow(messageId, point)
+    }
+
+    private fun showPopupWindow(
+        @StringRes messageId: Int,
+        point: Point,
+    ) {
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val parentViewGroup = findViewById<ViewGroup>(android.R.id.content)
+        val binding = LayoutMissingTooltipBinding.inflate(inflater, parentViewGroup, false)
+        val popupView = binding.root
+        binding.tvTooltip.text = getString(messageId)
+
+        val popupWindow =
+            PopupWindow(
+                popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true,
+            )
+
+        popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val popupWidth = popupView.measuredWidth
+        val popupHeight = popupView.measuredHeight
+
+        val adjustedX = point.x - popupWidth
+        val adjustedY = point.y - popupHeight
+
+        popupWindow.showAtLocation(window.decorView, Gravity.NO_GRAVITY, adjustedX, adjustedY)
     }
 }
