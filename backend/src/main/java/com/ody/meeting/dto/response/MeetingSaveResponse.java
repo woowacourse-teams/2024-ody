76bf34f40f04b23a7d9a1fd7efbb3c940190c6e7
H <dmsgktn0208@gmail.com>
2024-08-06 15:52:08 +0900

    fix: 시간 데이터 초 제거 후 반환하도록 수정

--- backend/src/main/java/com/ody/meeting/dto/response/MeetingSaveResponse.java
+++ backend/src/main/java/com/ody/meeting/dto/response/MeetingSaveResponse.java
@@ -1,5 +1,6 @@
 package com.ody.meeting.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ody.mate.domain.Mate;
 import com.ody.mate.dto.response.MateResponse;
 import com.ody.meeting.domain.Meeting;
@@ -21,6 +22,7 @@ public record MeetingSaveResponse(
         LocalDate date,
 
         @Schema(description = "모임 시간", type = "string", example = "14:00")
+        @JsonFormat(pattern = "HH:mm")
         LocalTime time,
 
         @Schema(description = "도착지 주소", example = "서울 송파구 올림픽로35다길 42")
