390e6384f03601be9036e44785de30b5164b37fb
kimhyemin <kimhm0728@naver.com>
2024-08-07 23:25:45 +0900

    chore: 불필요한 테스트 제거

--- android/app/src/test/java/com/woowacourse/ody/domain/validator/AddressValidatorTest.kt
+++ android/app/src/test/java/com/woowacourse/ody/domain/validator/AddressValidatorTest.kt
@@ -1,17 +1,10 @@
 package com.woowacourse.ody.domain.validator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class AddressValidatorTest {
-    @Test
-    fun `주소가 수도권인 경우 유효하다2`() {
-        val actual = AddressValidator.isValid("인천광역시 남동구")
-        assertThat(actual).isTrue
-    }
-
     @ParameterizedTest
     @ValueSource(strings = ["인천광역시 남동구", "서울특별시 강남구", "경기도 수원시"])
     fun `주소가 수도권인 경우 유효하다`(address: String) {
