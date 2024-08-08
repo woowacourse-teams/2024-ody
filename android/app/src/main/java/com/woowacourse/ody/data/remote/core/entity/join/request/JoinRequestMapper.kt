04088f9cb0b123de86a859fa8d6340e230d45b40
haeum808 <haeumueah@gmail.com>
2024-08-08 20:37:54 +0900

    fix: compress 메서드 좌표 길이에 상관없이 안전하게 자르게 변경

--- android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/join/request/JoinRequestMapper.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/join/request/JoinRequestMapper.kt
@@ -2,6 +2,8 @@ package com.woowacourse.ody.data.remote.core.entity.join.request
 
 import com.woowacourse.ody.domain.model.MeetingJoinInfo
 
+private val COORDINATE_RANGE = (0..8)
+
 fun MeetingJoinInfo.toJoinRequest(): JoinRequest =
     JoinRequest(
         inviteCode = inviteCode,
@@ -11,4 +13,10 @@ fun MeetingJoinInfo.toJoinRequest(): JoinRequest =
         originLongitude = compress(originLongitude),
     )
 
-private fun compress(coordinate: String): String = coordinate.slice(0..8)
+private fun compress(coordinate: String): String {
+    return if (coordinate.length >= 9) {
+        coordinate.slice(COORDINATE_RANGE)
+    } else {
+        coordinate
+    }
+}
