a42779ee1f017cebb2a7e90ac67774358bb14808
mzeong <mjeong@sookmyung.ac.kr>
2024-08-08 11:07:51 +0900

    feat: exceptionHandler, interceptor 로그 추가
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- backend/src/main/java/com/ody/common/config/WebConfig.java
+++ backend/src/main/java/com/ody/common/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.ody.common.config;
 
 import com.ody.common.argumentresolver.AuthMemberArgumentResolver;
+import com.ody.common.interceptor.LoggingInterceptor;
 import com.ody.member.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,8 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final MemberService memberService;
 
+    private final LoggingInterceptor loggingInterceptor;
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthMemberArgumentResolver(memberService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
     }
 }
