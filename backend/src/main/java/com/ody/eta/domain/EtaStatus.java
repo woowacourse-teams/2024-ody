b1fb7a9f0ac50ed56d02a7bae4dc3ba71775a324
H <99153215+hyeon0208@users.noreply.github.com>
2024-08-08 21:20:42 +0900

    fix: 도착한 상태인 사람의 소요시간이 -1로 반환되고 있는 문제 해결 (#325)

--- backend/src/main/java/com/ody/eta/domain/EtaStatus.java
+++ backend/src/main/java/com/ody/eta/domain/EtaStatus.java
@@ -12,24 +12,21 @@ public enum EtaStatus {
     ;
 
     public static EtaStatus from(String ownerNickname, Eta mateEta, LocalDateTime meetingTime, LocalDateTime now, boolean isMissing) {
-        if (ownerNickname.equals(mateEta.getMate().getNicknameValue()) && (isMissing || mateEta.getRemainingMinutes() == -1L)) {
+        if (ownerNickname.equals(mateEta.getMate().getNicknameValue()) && isMissing || mateEta.getRemainingMinutes() == -1L) {
             return MISSING;
         }
         if (mateEta.isArrived()) {
             return ARRIVED;
         }
-
         if (!mateEta.willBeLate(meetingTime) && (now.isBefore(meetingTime))) {
             return ARRIVAL_SOON;
         }
-
         if (mateEta.willBeLate(meetingTime)) {
             if (now.isBefore(meetingTime)) {
                 return LATE_WARNING;
             }
             return LATE;
         }
-
         throw new OdyServerErrorException("참여자의 ETA 상태를 판단할 수 없습니다");
     }
 }
