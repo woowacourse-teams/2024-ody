151cc9a15a04c488069aef2745098b7addc55ddd
haeum808 <haeumueah@gmail.com>
2024-08-10 16:39:44 +0900

    refactor: PermissionHelper에 권한 요청하는 로직 추가

--- android/app/src/main/java/com/woowacourse/ody/presentation/common/PermissionHelper.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/common/PermissionHelper.kt
@@ -1,12 +1,48 @@
 package com.woowacourse.ody.presentation.common
 
 import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.woowacourse.ody.presentation.meetings.MeetingsActivity
 
 class PermissionHelper(private val context: Context) {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    fun requestNotificationPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+            NOTIFICATION_REQUEST_CODE,
+        )
+    }
+
+    fun coarseAndFineLocationPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            COARSE_AND_FINE_LOCATION_REQUEST_CODE,
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun requestBackgroundLocationPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            ),
+            BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE,
+        )
+    }
+
     fun hasCoarseLocationPermission(): Boolean {
         return ContextCompat.checkSelfPermission(
             context,
@@ -41,5 +77,13 @@ class PermissionHelper(private val context: Context) {
         } else {
             true
         }
+    }
+
+    companion object {
+        const val NOTIFICATION_REQUEST_CODE = 1001
+        const val COARSE_AND_FINE_LOCATION_REQUEST_CODE = 1002
+        const val BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE = 1003
+
+        fun getIntent(context: Context): Intent = Intent(context, MeetingsActivity::class.java)
     }
 }
--- android/app/src/main/java/com/woowacourse/ody/presentation/meetings/MeetingsActivity.kt
+++ android/app/src/main/java/com/woowacourse/ody/presentation/meetings/MeetingsActivity.kt
@@ -1,6 +1,5 @@
 package com.woowacourse.ody.presentation.meetings
 
-import android.Manifest
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
@@ -11,7 +10,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import com.woowacourse.ody.R
 import com.woowacourse.ody.databinding.ActivityMeetingsBinding
 import com.woowacourse.ody.presentation.common.PermissionHelper
@@ -25,9 +23,7 @@ import com.woowacourse.ody.presentation.room.etadashboard.EtaDashboardActivity
 import com.woowacourse.ody.presentation.room.log.NotificationLogActivity
 
 class MeetingsActivity :
-    BindingActivity<ActivityMeetingsBinding>(
-        R.layout.activity_meetings,
-    ),
+    BindingActivity<ActivityMeetingsBinding>(R.layout.activity_meetings),
     MeetingsListener {
     private val viewModel by viewModels<MeetingsViewModel> {
         MeetingsViewModelFactory(
@@ -134,15 +130,40 @@ class MeetingsActivity :
             return
         }
 
-        val permissions =
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.POST_NOTIFICATIONS,
-            )
+        permissionHelper.requestNotificationPermission(this)
+    }
 
-        val permissionRequestCode = PERMISSIONS_REQUEST_CODE
-        ActivityCompat.requestPermissions(this, permissions, permissionRequestCode)
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PermissionHelper.NOTIFICATION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] != PackageManager.PERMISSION_GRANTED
+            ) {
+                showSnackBar(R.string.meetings_notification_permission_required)
+            } else {
+                permissionHelper.coarseAndFineLocationPermission(this)
+            }
+        } else if (requestCode == PermissionHelper.COARSE_AND_FINE_LOCATION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                showBackgroundLocationPermissionDialog(this)
+            } else {
+                showSnackBar(R.string.meetings_location_permission_required)
+            }
+        } else if (requestCode == PermissionHelper.BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] != PackageManager.PERMISSION_GRANTED
+            ) {
+                showSnackBar(R.string.meetings_location_permission_required)
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -152,13 +173,7 @@ class MeetingsActivity :
             DialogInterface.OnClickListener { _, which ->
                 when (which) {
                     DialogInterface.BUTTON_POSITIVE ->
-                        ActivityCompat.requestPermissions(
-                            this,
-                            arrayOf(
-                                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                            ),
-                            BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE,
-                        )
+                        permissionHelper.requestBackgroundLocationPermission(this)
                 }
             }
         builder.setTitle(getString(R.string.request_background_permission_dialog_title))
@@ -170,32 +185,8 @@ class MeetingsActivity :
         builder.show()
     }
 
-    @RequiresApi(Build.VERSION_CODES.Q)
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray,
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
-            ) {
-                showBackgroundLocationPermissionDialog(this)
-            }
-        } else if (requestCode == BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] != PackageManager.PERMISSION_GRANTED
-            ) {
-                showSnackBar(R.string.meetings_location_permission_guide)
-            }
-        }
-    }
-
     companion object {
         private const val TAG = "MeetingsActivity"
-        private const val PERMISSIONS_REQUEST_CODE = 1
-        private const val BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE = 2
 
         fun getIntent(context: Context): Intent = Intent(context, MeetingsActivity::class.java)
     }
