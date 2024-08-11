2c5dab8102e6e220023c275359c48613631b71f3
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-11 03:01:29 -0700

    refactor: response. requset api 순서에 맞게 맞추기, 패키지 위치 수정 (#340)
    * chore: MateEtaInfoResponse 위치 변경
    
    * chore: 패키지 위치 변경 및 api 순서에 맞게 변경

--- android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/meeting/request/MeetingRequest.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/meeting/request/MeetingRequest.kt
@@ -5,10 +5,16 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class MeetingRequest(
-    @Json(name = "name") val name: String,
-    @Json(name = "date") val date: String,
-    @Json(name = "time") val time: String,
-    @Json(name = "targetAddress") val targetAddress: String,
-    @Json(name = "targetLatitude") val targetLatitude: String,
-    @Json(name = "targetLongitude") val targetLongitude: String,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "date")
+    val date: String,
+    @Json(name = "time")
+    val time: String,
+    @Json(name = "targetAddress")
+    val targetAddress: String,
+    @Json(name = "targetLatitude")
+    val targetLatitude: String,
+    @Json(name = "targetLongitude")
+    val targetLongitude: String,
 )
--- android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/meeting/response/MeetingCatalogResponse.kt
+++ android/app/src/main/java/com/woowacourse/ody/data/remote/core/entity/meeting/response/MeetingCatalogResponse.kt
@@ -5,20 +5,20 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class MeetingCatalogResponse(
-    @Json(name = "date")
-    val date: String,
-    @Json(name = "durationMinutes")
-    val durationMinutes: Int,
     @Json(name = "id")
     val id: Long,
-    @Json(name = "mateCount")
-    val mateCount: Int,
     @Json(name = "name")
     val name: String,
-    @Json(name = "originAddress")
-    val originAddress: String,
-    @Json(name = "targetAddress")
-    val targetAddress: String,
+    @Json(name = "mateCount")
+    val mateCount: Int,
+    @Json(name = "date")
+    val date: String,
     @Json(name = "time")
     val time: String,
+    @Json(name = "targetAddress")
+    val targetAddress: String,
+    @Json(name = "originAddress")
+    val originAddress: String,
+    @Json(name = "durationMinutes")
+    val durationMinutes: Long,
 )
