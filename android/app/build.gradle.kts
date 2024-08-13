3a99e9592a272f83a1b76f56174420b1f4dd637c
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-08-13 11:04:52 +0900

    config: 앱 출시를 위한 수정 (#350)
    * config: VERSION.1_8 -> VERSION_17
    
    * config: versionCode 1 -> versionCode 4
    
    * config: ad id에 대한 설정 무시하게 설정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.woowacourse.ody"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -40,11 +40,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         buildConfig = true
