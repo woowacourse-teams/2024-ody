ab8e7bd4e19a3c7441de067a20658a55814b87bb
eun-byeol <joeunbyeol98@gmail.com>
2024-08-14 01:00:54 +0900

    test: 수도권 위경도 좌표 검증 테스트 코드 가독성 개선

--- backend/src/test/java/com/ody/common/validator/SupportRegionValidatorTest.java
+++ backend/src/test/java/com/ody/common/validator/SupportRegionValidatorTest.java
@@ -4,12 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import com.ody.common.annotation.SupportRegion;
-import com.ody.mate.dto.request.MateSaveRequest;
+import com.ody.eta.dto.request.MateEtaRequest;
 import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.Payload;
 import java.lang.annotation.Annotation;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,61 +17,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class SupportRegionValidatorTest {
 
-    private SupportRegionValidator supportRegionValidator;
+    private static SupportRegionValidator supportRegionValidator = new SupportRegionValidator();
 
-    @BeforeEach
-    void init() {
+    @BeforeAll
+    static void setUp() {
         supportRegionValidator = new SupportRegionValidator();
-        supportRegionValidator.initialize(new SupportRegion() {
-
-            @Override
-            public Class<? extends Annotation> annotationType() {
-                return SupportRegion.class;
-            }
-
-            @Override
-            public String latitudeFieldName() {
-                return "originLatitude";
-            }
-
-            @Override
-            public String longitudeFieldName() {
-                return "originLongitude";
-            }
-
-            @Override
-            public String message() {
-                return "수도권 내 위경도 좌표만 가능합니다.";
-            }
-
-            @Override
-            public Class<?>[] groups() {
-                return new Class[0];
-            }
-
-            @Override
-            public Class<? extends Payload>[] payload() {
-                return new Class[0];
-            }
-        });
+        supportRegionValidator.initialize(new SupportRegionStub());
     }
 
     @DisplayName("수도권 내 위경도 좌표이면 true, 아니면 false를 반환한다.")
     @ParameterizedTest
     @MethodSource("supportRegionTestCases")
     void isValid(String latitude, String longitude, boolean expected) {
-        MateSaveRequest mateSaveRequest = new MateSaveRequest(
-                "testInviteCode",
-                "조조",
-                "서울 강남구",
-                latitude,
-                longitude
-        );
+        MateEtaRequest mateEtaRequest = new MateEtaRequest(false, latitude, longitude);
 
-        boolean valid = supportRegionValidator.isValid(
-                mateSaveRequest,
-                mock(ConstraintValidatorContext.class)
-        );
+        boolean valid = supportRegionValidator.isValid(mateEtaRequest, mock(ConstraintValidatorContext.class));
 
         assertThat(valid).isEqualTo(expected);
     }
@@ -85,5 +45,38 @@ class SupportRegionValidatorTest {
                 Arguments.of("38.099416", "127.979525", false), // 강원
                 Arguments.of("39.031896", "125.769334", false) // 평양
         );
+    }
+
+    private static class SupportRegionStub implements SupportRegion {
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return SupportRegion.class;
+        }
+
+        @Override
+        public String latitudeFieldName() {
+            return "currentLatitude";
+        }
+
+        @Override
+        public String longitudeFieldName() {
+            return "currentLongitude";
+        }
+
+        @Override
+        public String message() {
+            return "수도권 내 위경도 좌표만 가능합니다.";
+        }
+
+        @Override
+        public Class<?>[] groups() {
+            return new Class[0];
+        }
+
+        @Override
+        public Class<? extends Payload>[] payload() {
+            return new Class[0];
+        }
     }
 }
