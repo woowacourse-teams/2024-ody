e98b92b36fbf53ebbf7a62c96f5c69f8cbc209b6
aprilgom <aprilgom@gmail.com>
2024-08-13 14:57:15 +0900

    refactor: sealed interface로 변경

--- android/app/src/main/java/com/woowacourse/ody/domain/apiresult/ApiResult.kt
+++ android/app/src/main/java/com/woowacourse/ody/domain/apiresult/ApiResult.kt
@@ -2,12 +2,12 @@ package com.woowacourse.ody.domain.apiresult
 
 import java.io.IOException
 
-sealed class ApiResult<out T> {
-    data class Success<T>(val data: T) : ApiResult<T>()
+sealed interface ApiResult<out T> {
+    data class Success<T>(val data: T) : ApiResult<T>
 
-    data class Failure(val code: Int, val errorMessage: String?) : ApiResult<Nothing>()
+    data class Failure(val code: Int, val errorMessage: String?) : ApiResult<Nothing>
 
-    data class NetworkError(val exception: IOException) : ApiResult<Nothing>()
+    data class NetworkError(val exception: IOException) : ApiResult<Nothing>
 
-    class Unexpected(val t: Throwable) : ApiResult<Nothing>()
+    class Unexpected(val t: Throwable) : ApiResult<Nothing>
 }
