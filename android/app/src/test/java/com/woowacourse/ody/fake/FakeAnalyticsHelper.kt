104327b9622d272393a8430ff80452efae7a05b7
kimhyemin <kimhm0728@naver.com>
2024-08-13 20:20:44 +0900

    style: ktLint 적용

--- android/app/src/test/java/com/woowacourse/ody/fake/FakeAnalyticsHelper.kt
+++ android/app/src/test/java/com/woowacourse/ody/fake/FakeAnalyticsHelper.kt
@@ -4,5 +4,8 @@ import android.os.Bundle
 import com.woowacourse.ody.presentation.common.analytics.AnalyticsHelper
 
 object FakeAnalyticsHelper : AnalyticsHelper {
-    override fun logEvent(eventName: String, bundle: Bundle) {}
+    override fun logEvent(
+        eventName: String,
+        bundle: Bundle,
+    ) {}
 }
