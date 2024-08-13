639df0b0dc45487b697bb55b07684f40157ce5e7
aprilgom <aprilgom@gmail.com>
2024-08-13 12:22:35 +0900

    refactor: Extension의 람다 이름 변경, func -> blockˆ

--- android/app/src/main/java/com/woowacourse/ody/domain/apiresult/ApiResultExtensions.kt
+++ android/app/src/main/java/com/woowacourse/ody/domain/apiresult/ApiResultExtensions.kt
@@ -1,36 +1,36 @@
 package com.woowacourse.ody.domain.apiresult
 
-fun <T> ApiResult<T>.onSuccess(func: (T) -> Unit): ApiResult<T> {
+fun <T> ApiResult<T>.onSuccess(block: (T) -> Unit): ApiResult<T> {
     if (this is ApiResult.Success) {
-        func(this.data)
+        block(this.data)
     }
     return this
 }
 
-fun <T> ApiResult<T>.onFailure(func: (code: Int, errorMessage: String?) -> Unit): ApiResult<T> {
+fun <T> ApiResult<T>.onFailure(block: (code: Int, errorMessage: String?) -> Unit): ApiResult<T> {
     if (this is ApiResult.Failure) {
-        func(this.code, this.errorMessage)
+        block(this.code, this.errorMessage)
     }
     return this
 }
 
-fun <T> ApiResult<T>.onNetworkError(func: (exception: Exception) -> Unit): ApiResult<T> {
+fun <T> ApiResult<T>.onNetworkError(block: (exception: Exception) -> Unit): ApiResult<T> {
     if (this is ApiResult.NetworkError) {
-        func(this.exception)
+        block(this.exception)
     }
     return this
 }
 
-fun <T> ApiResult<T>.onUnexpected(func: (t: Throwable) -> Unit): ApiResult<T> {
+fun <T> ApiResult<T>.onUnexpected(block: (t: Throwable) -> Unit): ApiResult<T> {
     if (this is ApiResult.Unexpected) {
-        func(this.t)
+        block(this.t)
     }
     return this
 }
 
-suspend fun <T, R> ApiResult<T>.map(func: suspend (T) -> R): ApiResult<R> {
+suspend fun <T, R> ApiResult<T>.map(block: suspend (T) -> R): ApiResult<R> {
     return when (this) {
-        is ApiResult.Success -> ApiResult.Success(func(this.data))
+        is ApiResult.Success -> ApiResult.Success(block(this.data))
         is ApiResult.Failure -> ApiResult.Failure(this.code, this.errorMessage)
         is ApiResult.NetworkError -> ApiResult.NetworkError(this.exception)
         is ApiResult.Unexpected -> ApiResult.Unexpected(this.t)
