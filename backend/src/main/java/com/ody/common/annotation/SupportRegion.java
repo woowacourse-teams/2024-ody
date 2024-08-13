d11ab7b3803d7fc692fcecf3e67901927427a7e1
eun-byeol <joeunbyeol98@gmail.com>
2024-08-14 01:01:25 +0900

    refactor: 불필요한 괄호 제거

--- backend/src/main/java/com/ody/common/annotation/SupportRegion.java
+++ backend/src/main/java/com/ody/common/annotation/SupportRegion.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = SupportRegionValidator.class)
-@Target({ElementType.TYPE})
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SupportRegion {
 
