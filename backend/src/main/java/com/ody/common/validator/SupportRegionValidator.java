ce4588d1f55934e0721af4ce26721219c5ea2785
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2024-08-15 00:10:31 +0900

    feat: 약속 참여자 ETA 조회 시, 수도권 외 지역 예외처리 (#368)
    * feat: 커스텀 어노테이션으로 수도권 위경도 좌표 검증 로직 구현
    
    * feat: 수도권 위경도 좌표 검증 로직 Request Dto에 적용
    
    * test: 수도권 위경도 좌표 검증 테스트 코드 가독성 개선
    
    * refactor: 불필요한 괄호 제거
    
    * refactor: 변수명 수정
    
    * fix: 컨트롤러에서 @SupportRegion 로직 타지 않는 문제 해결

---
+++ backend/src/main/java/com/ody/common/validator/SupportRegionValidator.java
@@ -0,0 +1,51 @@
+package com.ody.common.validator;
+
+import com.ody.common.annotation.SupportRegion;
+import com.ody.common.exception.OdyServerErrorException;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+
+public class SupportRegionValidator implements ConstraintValidator<SupportRegion, Object> {
+
+    private static final BigDecimal MIN_LATITUDE = new BigDecimal("36.88");
+    private static final BigDecimal MAX_LATITUDE = new BigDecimal("38.3");
+    private static final BigDecimal MIN_LONGITUDE = new BigDecimal("125.6");
+    private static final BigDecimal MAX_LONGITUDE = new BigDecimal("127.9");
+
+    private String latitudeFieldName;
+    private String longitudeFieldName;
+
+    @Override
+    public void initialize(SupportRegion constraintAnnotation) {
+        latitudeFieldName = constraintAnnotation.latitudeFieldName();
+        longitudeFieldName = constraintAnnotation.longitudeFieldName();
+    }
+
+    @Override
+    public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            Class<?> objectClass = object.getClass();
+            Method latitudeGetter = objectClass.getMethod(latitudeFieldName);
+            Method longitudeGetter = objectClass.getMethod(longitudeFieldName);
+
+            String latitude = (String) latitudeGetter.invoke(object);
+            String longitude = (String) longitudeGetter.invoke(object);
+
+            return isInLatitudeRange(latitude) && isInLongitudeRange(longitude);
+        } catch (Exception exception) {
+            throw new OdyServerErrorException(exception.getMessage());
+        }
+    }
+
+    private boolean isInLatitudeRange(String latitude) {
+        BigDecimal latitudeValue = new BigDecimal(latitude);
+        return MIN_LATITUDE.compareTo(latitudeValue) <= 0 && MAX_LATITUDE.compareTo(latitudeValue) >= 0;
+    }
+
+    private boolean isInLongitudeRange(String longitude) {
+        BigDecimal longitudeValue = new BigDecimal(longitude);
+        return MIN_LONGITUDE.compareTo(longitudeValue) <= 0 && MAX_LONGITUDE.compareTo(longitudeValue) >= 0;
+    }
+}
