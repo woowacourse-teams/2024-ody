001d2b7bc3e1a96b6dad0ee372da209845e6f72c
coli-geonwoo <coli-geonwoo@gmail.com>
2024-08-14 16:54:05 +0900

    feat: 불변 값 객체로 전환

---
+++ backend/src/main/java/com/ody/notification/domain/message/PushMessage.java
@@ -0,0 +1,18 @@
+package com.ody.notification.domain.message;
+
+import com.google.firebase.messaging.Message;
+import com.ody.notification.domain.Notification;
+import lombok.Getter;
+
+@Getter
+public class PushMessage {
+    private final Message message;
+
+    public PushMessage(String topic, Notification notification){
+        this.message= Message.builder()
+                .putData("type", notification.getType().toString())
+                .putData("nickname", notification.getMate().getNicknameValue())
+                .setTopic(topic)
+                .build();
+    }
+}
