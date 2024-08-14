90f537f287b696a4644cc29c21d22ca0d81d4560
mzeong <mjeong@sookmyung.ac.kr>
2024-08-14 10:16:12 +0900

    chore: 로깅 프레임워크 적용 및 모니터링 대시보드 구성 (#348)
    * chore: docker run 시에 volume 설정 추가
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: 불필요한 env 제거
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: 로그 파일 생성을 위해 Logback 설정
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: commons-logging 제외
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * feat: exceptionHandler, interceptor 로그 추가
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: backend cd 스크립트 push 브랜치 수정
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: backend cd 스크립트 docker volume 수정
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: 로그 파일 gitignore에 추가
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: amount path 절대 경로로 수정
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: 로그 파일 절대 경로로 수정
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: 현재 작업중인 브랜치로 checkout하도록 임시 수정
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    * chore: profile에 따라 파일 경로 변경
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: 푸시 시 배포 적용되는 브랜치 수정
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: interceptor에서 request body 로그 출력
    
    - wrapper, filter 생성
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: interceptor에서 response body 로그 출력
    
    - wrapper 생성
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: 로그 개행 제거, 정상 요청에 대한 response body 로그 추가
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: ContentCachingResponseWrapper 사용해 response body 로그 추가
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: ContentCachingRequestWrapper 사용해 request body 로그 추가
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>
    
    * chore: cd workflow trigger 이벤트 수정
    
    Co-authored-by: mzeong <mjeong@sookmyung.ac.kr>
    
    ---------
    
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- backend/src/main/java/com/ody/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ody/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ody.common.exception;
 
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -16,16 +18,19 @@ public class GlobalExceptionHandler {
         String exceptionMessage = exception.getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(" | "));
+        log.warn("message: {}", exceptionMessage);
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exceptionMessage);
     }
 
     @ExceptionHandler(OdyException.class)
     public ProblemDetail handleCustomException(OdyException exception) {
+        log.warn("message: {}", exception.getMessage());
         return ProblemDetail.forStatusAndDetail(exception.getHttpStatus(), exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(Exception exception) {
+        log.error("exception: {}", exception);
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
     }
 }
