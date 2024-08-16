9b909e89fdf2f555eda47fef76e0a60c88da4789
coli-geonwoo <coli-geonwoo@gmail.com>
2024-08-16 11:46:13 +0900

    feat: errorCode400 description 추가

--- backend/src/main/java/com/ody/mate/controller/MateControllerSwagger.java
+++ backend/src/main/java/com/ody/mate/controller/MateControllerSwagger.java
@@ -61,7 +61,7 @@ public interface MateControllerSwagger {
             summary = "참여자 콕 찌르기",
             responses = {@ApiResponse(responseCode = "200", description = "콕 찌르기 성공")}
     )
-    @ErrorCode400
+    @ErrorCode400(description = "유효하지 않은 mateId")
     @ErrorCode401
     @ErrorCode500
     ResponseEntity<Void> nudgeMate(@Parameter(name = "참여자 Id", required = true) Long mateId);
