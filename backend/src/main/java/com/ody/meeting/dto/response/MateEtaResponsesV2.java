fd48ae275e659c96d3612b3a461b30abae1654a4
mzeong <mjeong@sookmyung.ac.kr>
2024-08-16 16:17:55 +0900

    docs: 약속 참여자 도착 현황 조회 API 문서화

---
+++ backend/src/main/java/com/ody/meeting/dto/response/MateEtaResponsesV2.java
@@ -0,0 +1,20 @@
+package com.ody.meeting.dto.response;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Arrays;
+import java.util.List;
+
+public record MateEtaResponsesV2(
+
+        @Schema(description = "요청자의 참여자 아이디", example = "4")
+        long requesterMateId,
+
+        @ArraySchema(schema = @Schema(implementation = MateEtaResponseV2.class))
+        List<MateEtaResponseV2> mateEtas
+) {
+
+    public MateEtaResponsesV2(long requesterMateId, MateEtaResponseV2... mateEtaResponseV2) {
+        this(requesterMateId, Arrays.asList(mateEtaResponseV2));
+    }
+}
