dcf6537bcffb3c884267a3ba90dfde8abc51d70a
mzeong <mjeong@sookmyung.ac.kr>
2024-08-18 17:53:50 +0900

    feat: 임의의 `Provider`를 가진 회원이 존재하는지 조회
    Co-authored-by: coli-geonwoo <jameskkwoo1021@gmail.com>

--- backend/src/main/java/com/ody/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ody/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.ody.member.repository;
 
+import com.ody.member.domain.AuthProvider;
 import com.ody.member.domain.DeviceToken;
 import com.ody.member.domain.Member;
 import java.util.Optional;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findFirstByDeviceToken(DeviceToken deviceToken);
+
+    boolean existsByAuthProvider(AuthProvider authProvider);
 }
