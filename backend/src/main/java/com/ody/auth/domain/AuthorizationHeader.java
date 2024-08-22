c1c9cba327e11176e255b54e4c471e62cb1d355d
mzeong <mjeong@sookmyung.ac.kr>
2024-08-22 10:56:13 +0900

    refactor: 사용하지 않는 AuthorizationHeader 내 상수 제거

--- backend/src/main/java/com/ody/auth/domain/AuthorizationHeader.java
+++ backend/src/main/java/com/ody/auth/domain/AuthorizationHeader.java
@@ -2,16 +2,12 @@ package com.ody.auth.domain;
 
 import com.ody.auth.token.AccessToken;
 import com.ody.auth.token.RefreshToken;
-import com.ody.common.exception.OdyBadRequestException;
 import com.ody.common.exception.OdyUnauthorizedException;
 import lombok.Getter;
 
 @Getter
 public class AuthorizationHeader {
 
-    public static final String TOKEN_TYPE = "Bearer";
-    private static final String ACCESS_TOKEN_PREFIX = "access-token=";
-    public static final String REFRESH_TOKEN_PREFIX = "refresh-token=";
     public static final String DELIMITER = " ";
 
     private final AccessToken accessToken;
