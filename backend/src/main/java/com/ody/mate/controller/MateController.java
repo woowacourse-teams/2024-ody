7e006c3cb7aff2104b02bf1bea83791d950bf3d8
coli-geonwoo <148152234+coli-geonwoo@users.noreply.github.com>
2024-08-22 15:33:33 +0900

    refactor: 로그 목록 조회, 약속 단건 조회 API에 imageUrl 필드 추가 (#461)
    * feat: 약속 단건 조회에 imageUrl 추가
    
    * feat: 로그 목록 조회에 imageUrl추가
    
    * chore: 사용하지 않은 API 제거
    
    * test: v1/mates 테스트 삭제
    
    ---------
    
    Co-authored-by: coli-geonwoo <coli-geonwoo@gmail.com>

--- backend/src/main/java/com/ody/mate/controller/MateController.java
+++ backend/src/main/java/com/ody/mate/controller/MateController.java
@@ -1,17 +1,13 @@
 package com.ody.mate.controller;
 
 import com.ody.common.annotation.AuthMember;
-import com.ody.mate.dto.request.MateSaveRequest;
 import com.ody.mate.dto.request.MateSaveRequestV2;
 import com.ody.mate.dto.request.NudgeRequest;
-import com.ody.mate.dto.response.MateSaveResponse;
 import com.ody.mate.dto.response.MateSaveResponseV2;
 import com.ody.mate.service.MateService;
 import com.ody.meeting.service.MeetingService;
 import com.ody.member.domain.Member;
 import jakarta.validation.Valid;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,26 +21,6 @@ public class MateController implements MateControllerSwagger {
 
     private final MeetingService meetingService;
     private final MateService mateService;
-
-    @Override
-    @PostMapping("/v1/mates")
-    public ResponseEntity<MateSaveResponse> saveV1(
-            @AuthMember Member member,
-            @Valid @RequestBody MateSaveRequest mateSaveRequest
-    ) {
-        MateSaveResponse mateSaveResponse = new MateSaveResponse(
-                1L,
-                "우테코 16조",
-                LocalDate.parse("2024-07-15"),
-                LocalTime.parse("14:00"),
-                "서울 송파구 올림픽로35다길 42",
-                "37.515298",
-                "127.103113",
-                "초대코드"
-        );
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(mateSaveResponse);
-    }
 
     @Override
     @PostMapping("/v2/mates")
