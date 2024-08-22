e412db9445e0388cf2e1f2fc1e89dcefc366c7da
kimhyemin <kimhm0728@naver.com>
2024-08-22 15:18:34 +0900

    refactor: BASE_URL 빌드 모드에 따른 분기 처리

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "BASE_URL", properties["BASE_URL"].toString())
+        buildConfigField("String", "BASE_PROD_URL", properties["BASE_PROD_URL"].toString())
+        buildConfigField("String", "BASE_DEV_URL", properties["BASE_DEV_URL"].toString())
         buildConfigField("String", "KAKAO_API_KEY", properties["KAKAO_API_KEY"].toString())
         buildConfigField("String", "KAKAO_NATIVE_KEY", properties["KAKAO_NATIVE_KEY"].toString())
         manifestPlaceholders["KAKAO_NATIVE_KEY"] = properties["KAKAO_NATIVE_KEY"].toString()
