ee859f2995be218f187830ebed012ac161b48f6a
mzeong <mjeong@sookmyung.ac.kr>
2024-09-06 00:43:01 +0900

    docs: 회원 삭제 API

--- backend/src/main/java/com/ody/member/controller/MemberController.java
+++ backend/src/main/java/com/ody/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,13 @@ public class MemberController implements MemberControllerSwagger {
     public ResponseEntity<Void> save(@RequestHeader("Authorization") String authorization) {
         memberService.save(new DeviceToken(authorization));
         return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
+    }
+
+    @Override
+    @DeleteMapping("/members")
+    public ResponseEntity<Void> delete(String authorization) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .build();
     }
 }
