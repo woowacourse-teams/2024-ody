5a3ee88807286ee2ca09555cf4c74e6908027b6e
haeum808 <haeumueah@gmail.com>
2024-09-09 14:43:03 +0900

    feat: 개인정보 처리 방침, 서비스 이용 약관 설정에서 링크 연결

--- android/app/src/main/java/com/mulberry/ody/presentation/setting/SettingActivity.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/setting/SettingActivity.kt
@@ -2,6 +2,7 @@ package com.mulberry.ody.presentation.setting
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.LinearLayout
 import com.google.android.material.divider.MaterialDividerItemDecoration
@@ -36,9 +37,7 @@ class SettingActivity :
                 dividerInsetStart = dpToPx(SETTING_ITEM_HORIZONTAL_MARGIN_DP)
                 dividerInsetEnd = dpToPx(SETTING_ITEM_HORIZONTAL_MARGIN_DP)
             }
-
         binding.rvSetting.addItemDecoration(dividerItemDecoration)
-
         adapter.submitList(SettingUiModel.entries)
     }
 
@@ -47,9 +46,13 @@ class SettingActivity :
     override fun onClickSettingItem(settingUiModel: SettingUiModel) {
         when (settingUiModel) {
             SettingUiModel.PRIVACY_POLICY -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(PRIVACY_POLICY_URI))
+                startActivity(intent)
             }
 
             SettingUiModel.TERM -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(TERM_URI))
+                startActivity(intent)
             }
 
             SettingUiModel.LOGOUT -> {
@@ -67,6 +70,10 @@ class SettingActivity :
 
     companion object {
         private const val SETTING_ITEM_HORIZONTAL_MARGIN_DP = 26
+        private const val PRIVACY_POLICY_URI =
+            "https://sly-face-106.notion.site/fecbe589eb23471ba2d0685cb3c2d274?pvs=4"
+        private const val TERM_URI =
+            "https://sly-face-106.notion.site/beb204b6e6724ecbbb83496448fc7b53?pvs=4"
 
         fun getIntent(context: Context): Intent = Intent(context, SettingActivity::class.java)
     }
