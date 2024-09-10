e40b9ac0dff5a2f15f0a70d426bb29eb58e4c082
H <dmsgktn0208@gmail.com>
2024-09-11 02:00:16 +0900

    refactor: local, dev, prod 프로필에만 jasypt 설정 적용되도록 설정

--- backend/src/main/java/com/ody/common/config/JasyptConfig.java
+++ backend/src/main/java/com/ody/common/config/JasyptConfig.java
@@ -6,8 +6,10 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({"local", "dev", "prod"})
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
