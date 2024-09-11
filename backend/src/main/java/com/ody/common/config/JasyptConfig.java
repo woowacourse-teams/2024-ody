00ca2486db94238e6ca6bfa1773227322bb474c2
H <dmsgktn0208@gmail.com>
2024-09-11 14:31:09 +0900

    refactor: 어노테이션 순서 변경

--- backend/src/main/java/com/ody/common/config/JasyptConfig.java
+++ backend/src/main/java/com/ody/common/config/JasyptConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
 @Profile({"local", "dev", "prod"})
+@Configuration
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
