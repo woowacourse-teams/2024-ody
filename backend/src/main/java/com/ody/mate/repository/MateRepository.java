3dfabee2560607c5c8be964796fd91dbb44f454b
H <dmsgktn0208@gmail.com>
2024-09-11 19:55:40 +0900

    refactor: 쿼리 변수 네이밍 수정 및 순서 수정

--- backend/src/main/java/com/ody/mate/repository/MateRepository.java
+++ backend/src/main/java/com/ody/mate/repository/MateRepository.java
@@ -30,12 +30,12 @@ public interface MateRepository extends JpaRepository<Mate, Long> {
             select mate
             from Mate mate
             join fetch mate.meeting
-            where mate.member.id = :memberId
-            and mate.meeting.id = :meetingId
+            where mate.meeting.id = :meetingId
+            and mate.member.id = :memberId
             """)
     Optional<Mate> findByMeetingIdAndMemberId(Long meetingId, Long memberId);
 
-    boolean existsByMeetingIdAndMemberId(Long memberId, Long meetingId);
+    boolean existsByMeetingIdAndMemberId(Long meetingId, Long memberId);
 
-    int countByMeetingId(Long id);
+    int countByMeetingId(Long meetingId);
 }
