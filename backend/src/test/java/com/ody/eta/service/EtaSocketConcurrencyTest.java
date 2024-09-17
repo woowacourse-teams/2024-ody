0c519dd88214597df0e2d55f9956f5fa85320f86
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-18 02:33:34 +0900

    test: await 시간 제한 지정

--- backend/src/test/java/com/ody/eta/service/EtaSocketConcurrencyTest.java
+++ backend/src/test/java/com/ody/eta/service/EtaSocketConcurrencyTest.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -88,7 +89,7 @@ class EtaSocketConcurrencyTest extends BaseServiceTest {
             signal.countDown();
         }
 
-        signal.await(); //signal이 0이 되면 넘어감
+        signal.await(3, TimeUnit.SECONDS); //signal이 0이 되면 넘어감
         executorService.shutdown();
     }
 }
