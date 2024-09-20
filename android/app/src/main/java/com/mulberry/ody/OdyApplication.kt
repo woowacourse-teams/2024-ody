a734f079b12418233c9920770418be0fd8284d22
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-20 23:39:40 +0900

    del: 불필요한 코드 삭제

--- android/app/src/main/java/com/mulberry/ody/OdyApplication.kt
+++ android/app/src/main/java/com/mulberry/ody/OdyApplication.kt
@@ -8,68 +8,6 @@ import timber.log.Timber
 
 @HiltAndroidApp
 class OdyApplication : Application() {
-//    private val odyDatastore by lazy {
-//        OdyDatastore(this)
-//    }
-
-//    private val refreshTokenService: Lazy<RefreshTokenService> by lazy {
-//        Lazy {
-//            refreshRetrofit.create(RefreshTokenService::class.java)
-//        }
-//    }
-
-//    val authTokenRepository: AuthTokenRepository by lazy {
-//        DefaultAuthTokenRepository(
-//            refreshTokenService.get(),
-//            odyDatastore,
-//        )
-//    }
-
-//    private val retrofit: Retrofit = RetrofitClient(authTokenRepository).retrofit
-//    private val loginRetrofit: Retrofit = RetrofitClient(authTokenRepository).loginRetrofit
-//    private val refreshRetrofit: Retrofit = RetrofitClient(authTokenRepository).refreshRetrofit
-//    private val kakaoRetrofit: Retrofit = KakaoRetrofitClient().retrofit
-
-//    private val joinService: JoinService = retrofit.create(JoinService::class.java)
-//    private val meetingService: MeetingService = retrofit.create(MeetingService::class.java)
-//    private val loginService: LoginService = loginRetrofit.create(LoginService::class.java)
-//    private val logoutService: LogoutService = retrofit.create(LogoutService::class.java)
-//    private val notificationService: NotificationService = retrofit.create(NotificationService::class.java)
-//    private val kakaoLocationService: KakaoLocationService =
-//        kakaoRetrofit.create(KakaoLocationService::class.java)
-
-//    private val workerManager: WorkManager by lazy { WorkManager.getInstance(this) }
-//    val analyticsHelper: AnalyticsHelper by lazy { FirebaseAnalyticsHelper(this) }
-//    val notificationHelper: NotificationHelper by lazy { NotificationHelper(this) }
-//    val permissionHelper: PermissionHelper by lazy { PermissionHelper(this) }
-//    val imageStorage: ImageStorage = FirebaseImageStorage()
-
-//    val joinRepository: JoinRepository by lazy { DefaultJoinRepository(joinService) }
-//    val meetingRepository: MeetingRepository by lazy { DefaultMeetingRepository(meetingService) }
-//    val fcmTokenRepository: FCMTokenRepository by lazy { DefaultFCMTokenRepository(odyDatastore) }
-//    val matesEtaRepository: MatesEtaRepository by lazy { DefaultMatesEtaRepository(workerManager) }
-//    val notificationLogRepository: NotificationLogRepository by lazy {
-//        DefaultNotificationLogRepository(
-//            notificationService,
-//        )
-//    }
-
-//    val kakaoGeoLocationRepository: KakaoGeoLocationRepository by lazy {
-//        KakaoGeoLocationRepository(
-//            kakaoLocationService,
-//        )
-//    }
-
-//    val kakaoLoginRepository: KakaoLoginRepository by lazy {
-//        KakaoLoginRepository(
-//            loginService,
-//            logoutService,
-//            odyDatastore,
-//            KakaoOAuthLoginService(),
-//            fcmTokenRepository,
-//        )
-//    }
-
     override fun onCreate() {
         super.onCreate()
 
