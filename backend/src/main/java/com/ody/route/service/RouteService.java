5f9119bf5b47e562b0c0e1807e7e1562c073da29
H <dmsgktn0208@gmail.com>
2024-09-22 18:22:29 +0900

    refactor: RouteService 런타임 시점에 의존성 변경 가능하도록 수정 및 로깅 추가

--- backend/src/main/java/com/ody/route/service/RouteService.java
+++ backend/src/main/java/com/ody/route/service/RouteService.java
@@ -13,17 +13,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RouteService {
 
-
     private final List<RouteClient> routeClients;
 
     public RouteTime calculateRouteTime(Coordinates origin, Coordinates target) {
         for (RouteClient client : routeClients) {
             try {
-                return client.calculateRouteTime(origin, target);
-            } catch (Exception e) {
-                log.warn("Route Client 에러 : {} ", client.getClass().getSimpleName());
+                RouteTime routeTime = client.calculateRouteTime(origin, target);
+                log.info("{}를 사용한 소요 시간 계산 성공", client.getClass().getSimpleName());
+                return routeTime;
+            } catch (Exception exception) {
+                log.warn("Route Client 에러 : {} ", client.getClass().getSimpleName(), exception);
             }
         }
-        throw new OdyServerErrorException("소요 시간 계산을 할 수 없습니다.");
+        log.error("모든 소요시간 계산 API 사용 불가");
+        throw new OdyServerErrorException("서버에 장애가 발생했습니다.");
     }
 }
