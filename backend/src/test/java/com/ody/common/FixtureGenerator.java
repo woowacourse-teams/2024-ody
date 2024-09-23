d27f9bf60e857a244b309898fd521c8e09376fc7
H <99153215+hyeon0208@users.noreply.github.com>
2024-09-23 14:21:16 +0900

    refactor: @Disabled 처리된 테스트 통과가능한 테스트로 수정 (#551)
    * refactor: 초대코드 하드 코딩 문자열을 InviteCodeGenerator로 생성된 초대코드로 변경
    
    * refactor: Test 환경에서만 적용되는 FixtureGeneratorConfig로 변경
    
    * refactor: 초대코드 테스트 @Disabled 제거
    
    * refactor: findAllByMember() 테스트 @Disabled 제거
    
    * refactor: FixtureGenerator에 회원의 엑세스 토큰 생성 로직 추가
    
    * refactor: BaseServiceTest 추상 클래스에 FixtureGeneratorConfig 임포트 적용
    
    * refactor: BaseRepositoryTest 추상 클래스에 TestAuthConfig 임포트 적용 및 test 프로필로 활성화
    
    * refactor: BaseControllerTest 추상 클래스에 FixtureGeneratorConfig 임포트 적용 및 미사용 메서드 제거
    
    * fix: 디미터 법칙 getter conflict 해결
    
    ---------
    
    Co-authored-by: coli-geonwoo <coli-geonwoo@gmail.com>

--- backend/src/test/java/com/ody/common/FixtureGenerator.java
+++ backend/src/test/java/com/ody/common/FixtureGenerator.java
@@ -1,5 +1,6 @@
 package com.ody.common;
 
+import com.ody.auth.JwtTokenProvider;
 import com.ody.auth.token.RefreshToken;
 import com.ody.eta.domain.Eta;
 import com.ody.eta.repository.EtaRepository;
@@ -28,19 +29,22 @@ public class FixtureGenerator {
     private final MateRepository mateRepository;
     private final NotificationRepository notificationRepository;
     private final EtaRepository etaRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public FixtureGenerator(
             MeetingRepository meetingRepository,
             MemberRepository memberRepository,
             MateRepository mateRepository,
             NotificationRepository notificationRepository,
-            EtaRepository etaRepository
+            EtaRepository etaRepository,
+            JwtTokenProvider jwtTokenProvider
     ) {
         this.meetingRepository = meetingRepository;
         this.memberRepository = memberRepository;
         this.mateRepository = mateRepository;
         this.notificationRepository = notificationRepository;
         this.etaRepository = etaRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public Meeting generateMeeting() {
@@ -107,5 +111,9 @@ public class FixtureGenerator {
                 notificationStatus,
                 new FcmTopic(mate.getMeeting())
         ));
+    }
+
+    public String generateAccessTokenValueByMember(Member member) {
+        return "Bearer access-token=" + jwtTokenProvider.createAccessToken(member.getId()).getValue();
     }
 }
