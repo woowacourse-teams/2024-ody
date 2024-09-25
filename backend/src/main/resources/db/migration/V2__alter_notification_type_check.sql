9f1b4d4f9fe5f34810a4a69e6d8e3a9707e1bc0a
H <dmsgktn0208@gmail.com>
2024-09-26 00:54:51 +0900

    chore: flyway V2 notification 체크 제약 조건 제거 구문 제거

--- backend/src/main/resources/db/migration/V2__alter_notification_type_check.sql
+++ backend/src/main/resources/db/migration/V2__alter_notification_type_check.sql
@@ -1,3 +1,1 @@
-ALTER TABLE notification DROP CONSTRAINT notification_chk_1;
-
 ALTER TABLE notification ADD CONSTRAINT notification_chk_1 CHECK (notification.type IN ('DEPARTURE_REMINDER', 'ENTRY', 'NUDGE', 'MEMBER_DELETION', 'ETA_NOTICE'));
