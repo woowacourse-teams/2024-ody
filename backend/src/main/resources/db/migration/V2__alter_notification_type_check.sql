f852ae60dae18312af68add9b77ab54e71ef17d5
H <dmsgktn0208@gmail.com>
2024-09-25 16:46:26 +0900

    chore: flyway 스키마 변경 사항 sql 추가

---
+++ backend/src/main/resources/db/migration/V2__alter_notification_type_check.sql
@@ -0,0 +1,3 @@
+ALTER TABLE notification DROP CONSTRAINT notification_chk_1;
+
+ALTER TABLE notification ADD CONSTRAINT notification_chk_1 CHECK (notification.type IN ('DEPARTURE_REMINDER', 'ENTRY', 'NUDGE', 'MEMBER_DELETION', 'ETA_NOTICE'));
