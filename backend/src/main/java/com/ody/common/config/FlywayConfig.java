af1c3191e99439b2bf38801ad98f5d3a202bb132
mzeong <mjeong@sookmyung.ac.kr>
2024-09-26 16:56:23 +0900

    feat: flyway repair

---
+++ backend/src/main/java/com/ody/common/config/FlywayConfig.java
@@ -0,0 +1,17 @@
+package com.ody.common.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayMigrationStrategy repairMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
