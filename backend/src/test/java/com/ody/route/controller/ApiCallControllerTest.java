d0c22bbc58a21858e535d6c75cce866338283450
eun-byeol <joeunbyeol98@gmail.com>
2024-10-04 17:51:00 +0900

    feat: ApiCallController 구현

---
+++ backend/src/test/java/com/ody/route/controller/ApiCallControllerTest.java
@@ -0,0 +1,33 @@
+package com.ody.route.controller;
+
+import com.ody.common.BaseControllerTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ApiCallControllerTest extends BaseControllerTest {
+
+    @DisplayName("ODsay API 호출 횟수 조회에 성공하면 200 응답을 반환한다")
+    @Test
+    void countOdsayApiCall() {
+        RestAssured.given().log().all()
+                .when()
+                .get("/admin/api-call/count/odsay")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
+    @DisplayName("Google Maps API 호출 횟수 조회에 성공하면 200 응답을 반환한다")
+    @Test
+    void countGoogleMapsApiCall() {
+        RestAssured.given().log().all()
+                .when()
+                .get("/admin/api-call/count/google-maps")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+}
