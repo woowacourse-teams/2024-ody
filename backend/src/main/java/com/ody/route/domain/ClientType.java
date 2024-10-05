496dbd24fb7cfdf7761f2b41cb85041ffcb48ed4
eun-byeol <joeunbyeol98@gmail.com>
2024-10-05 17:27:15 +0900

    feat: RouteClient 구현체에 따라 ClientType 반환하는 메서드 추가

--- backend/src/main/java/com/ody/route/domain/ClientType.java
+++ backend/src/main/java/com/ody/route/domain/ClientType.java
@@ -1,8 +1,19 @@
 package com.ody.route.domain;
 
+import com.ody.route.service.RouteClient;
+import java.util.Arrays;
+
 public enum ClientType {
 
     ODSAY,
     GOOGLE,
     ;
+
+    public static ClientType from(RouteClient routeClient) {
+        String className = routeClient.getClass().getSimpleName().toUpperCase();
+        return Arrays.stream(values())
+                .filter(clientType -> className.contains(clientType.name()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 ClientType 입니다."));
+    }
 }
