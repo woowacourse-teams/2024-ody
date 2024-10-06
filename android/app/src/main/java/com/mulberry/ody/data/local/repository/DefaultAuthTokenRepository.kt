760a1ed0eceee32a410aa20c65839cfe4a1cd35f
aprilgom <aprilgom@gmail.com>
2024-10-06 18:52:27 +0900

    refactor: refreshAuthToken 메서드를 AuthTokenRepository에서 AccessTokenInterceptor로 이동

--- android/app/src/main/java/com/mulberry/ody/data/local/repository/DefaultAuthTokenRepository.kt
+++ android/app/src/main/java/com/mulberry/ody/data/local/repository/DefaultAuthTokenRepository.kt
@@ -1,13 +1,8 @@
 package com.mulberry.ody.data.local.repository
 
 import com.mulberry.ody.data.local.db.OdyDatastore
-import com.mulberry.ody.data.remote.core.entity.login.mapper.toAuthToken
-import com.mulberry.ody.data.remote.core.service.RefreshTokenService
-import com.mulberry.ody.domain.apiresult.ApiResult
-import com.mulberry.ody.domain.apiresult.map
 import com.mulberry.ody.domain.model.AuthToken
 import com.mulberry.ody.domain.repository.ody.AuthTokenRepository
-import dagger.Lazy
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
@@ -15,18 +10,9 @@ class DefaultAuthTokenRepository
     @Inject
     constructor(
         private val odyDatastore: OdyDatastore,
-        private val refreshTokenService: Lazy<RefreshTokenService>,
     ) : AuthTokenRepository {
         override suspend fun fetchAuthToken(): Result<AuthToken> {
             return odyDatastore.getAuthToken().first()
-        }
-
-        override suspend fun refreshAuthToken(): ApiResult<AuthToken> {
-            return refreshTokenService.get().postRefreshToken().map {
-                val authToken = it.toAuthToken()
-                odyDatastore.setAuthToken(authToken)
-                authToken
-            }
         }
 
         override suspend fun removeAuthToken() {
