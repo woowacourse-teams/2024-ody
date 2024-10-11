c9fb583f106b681dc0d39dfb3174c1f0047b0db9
H <dmsgktn0208@gmail.com>
2024-10-11 17:13:45 +0900

    chore: V5 스크립트 주석 제거 및 필드명 수정

--- backend/src/main/resources/db/migration/V5__alter__timestamp__set-nano.sql
+++ backend/src/main/resources/db/migration/V5__alter__timestamp__set-nano.sql
@@ -1,10 +1,10 @@
-/*!ALTER TABLE member MODIFY COLUMN deleted_at timestamp(6);*/
-/*!ALTER TABLE meeting MODIFY COLUMN created_at timestamp(6);*/
-/*!ALTER TABLE meeting MODIFY COLUMN updated_at timestamp(6);*/
-/*!ALTER TABLE mate MODIFY COLUMN deleted_at timestamp(6);*/
-/*!ALTER TABLE eta CHANGE COLUMN createdAt firstApiCallAt timestamp not null;*/
-/*!ALTER TABLE eta CHANGE COLUMN updatedAt lastApiCallAt timestamp not null;*/
-/*!ALTER TABLE eta MODIFY COLUMN deleted_at timestamp(6);*/
-/*!ALTER TABLE notification MODIFY COLUMN send_at timestamp(6);*/
-/*!ALTER TABLE notification MODIFY COLUMN created_at timestamp(6);*/
-/*!ALTER TABLE notification MODIFY COLUMN updated_at timestamp(6);*/
+ALTER TABLE member MODIFY COLUMN deleted_at timestamp(6);
+ALTER TABLE meeting MODIFY COLUMN created_at timestamp(6);
+ALTER TABLE meeting MODIFY COLUMN updated_at timestamp(6);
+ALTER TABLE mate MODIFY COLUMN deleted_at timestamp(6);
+ALTER TABLE eta CHANGE COLUMN created_at first_api_call_at timestamp not null;
+ALTER TABLE eta CHANGE COLUMN updated_at last_api_call_at timestamp not null;
+ALTER TABLE eta MODIFY COLUMN deleted_at timestamp(6);
+ALTER TABLE notification MODIFY COLUMN send_at timestamp(6);
+ALTER TABLE notification MODIFY COLUMN created_at timestamp(6);
+ALTER TABLE notification MODIFY COLUMN updated_at timestamp(6);
