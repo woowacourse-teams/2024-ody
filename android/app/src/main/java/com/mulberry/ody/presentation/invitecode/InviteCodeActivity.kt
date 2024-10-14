7a6fc878617ed8d9323ca8ff891dd43851bf28d3
kimhyemin <kimhm0728@naver.com>
2024-10-14 16:36:27 +0900

    refactor: InviteCodeViewModel Flow로 리팩터링

--- android/app/src/main/java/com/mulberry/ody/presentation/invitecode/InviteCodeActivity.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/invitecode/InviteCodeActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.mulberry.ody.R
 import com.mulberry.ody.databinding.ActivityInviteCodeBinding
 import com.mulberry.ody.presentation.common.binding.BindingActivity
 import com.mulberry.ody.presentation.common.listener.BackListener
 import com.mulberry.ody.presentation.join.MeetingJoinActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class InviteCodeActivity : BindingActivity<ActivityInviteCodeBinding>(R.layout.activity_invite_code), BackListener {
@@ -27,17 +31,27 @@ class InviteCodeActivity : BindingActivity<ActivityInviteCodeBinding>(R.layout.a
     }
 
     private fun initializeObserve() {
-        viewModel.alreadyParticipatedEvent.observe(this) {
-            viewModel.clearInviteCode()
-            showSnackBar(R.string.invite_code_already_participated)
-        }
-        viewModel.invalidInviteCodeEvent.observe(this) {
-            viewModel.clearInviteCode()
-            showSnackBar(R.string.invite_code_invalid_invite_code)
-        }
-        viewModel.navigateAction.observe(this) {
-            navigateToJoinView()
-            finish()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.alreadyParticipatedEvent.collect {
+                        viewModel.clearInviteCode()
+                        showSnackBar(R.string.invite_code_already_participated)
+                    }
+                }
+                launch {
+                    viewModel.invalidInviteCodeEvent.collect {
+                        viewModel.clearInviteCode()
+                        showSnackBar(R.string.invite_code_invalid_invite_code)
+                    }
+                }
+                launch {
+                    viewModel.navigateAction.collect {
+                        navigateToJoinView()
+                        finish()
+                    }
+                }
+            }
         }
         viewModel.networkErrorEvent.observe(this) {
             showRetrySnackBar { viewModel.retryLastAction() }
