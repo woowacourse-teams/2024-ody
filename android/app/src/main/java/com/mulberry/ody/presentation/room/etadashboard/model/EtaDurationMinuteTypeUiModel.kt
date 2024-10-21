d59a32b00c8f4a9a47c18157699c8d7bba454255
kimhyemin <kimhm0728@naver.com>
2024-10-22 01:58:03 +0900

    refactor: eta status 매직 넘버 제거 및 리팩터링

--- android/app/src/main/java/com/mulberry/ody/presentation/room/etadashboard/model/EtaDurationMinuteTypeUiModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mulberry.ody.presentation.room.etadashboard.model
-
-enum class EtaDurationMinuteTypeUiModel {
-    ARRIVED,
-    MISSING,
-    ARRIVAL_SOON,
-    ARRIVAL_REMAIN_TIME,
-}
