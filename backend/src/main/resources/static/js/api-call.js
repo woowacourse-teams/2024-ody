381f6bab3e4e1857d6ff48bc522762e12afcf52c
mzeong <mjeong@sookmyung.ac.kr>
2024-10-22 09:48:49 +0900

    feat: 외부 API 어드민 페이지 구축 (#717)
    * feat: 어드민 페이지 html, js, css 작성
    
    - thymeleaf 의존성 추가
    
    * refactor: js 내에서 사용하지 않는 메서드 제거
    
    * style: spring-boot-starter 정렬
    
    * style: css 내 주석 제거
    
    * refactor: ApiCallPageController 메서드 이름 유의미하게 변경
    
    * refactor: 버튼 이름 의미 부여, enabled 조회해 버튼 갱신하도록 수정
    
    * feat: 어드민 페이지 주소 환경 변수 설정
    
    * refactor: axios 제거
    
    * fix: ApiCallPageController profile 설정
    
    * fix: 응답값 변환

---
+++ backend/src/main/resources/static/js/api-call.js
@@ -0,0 +1,110 @@
+const DEV_SERVER = 'https://dev.oody.site';
+const PROD_SERVER = 'https://prod.oody.site';
+const ODSAY = '/odsay';
+const GOOGLE = '/google';
+
+const COUNT_URL = '/admin/api-call/count';
+const TOGGLE_URL = '/admin/api-call/toggle';
+const ENABLED_URL = '/admin/api-call/enabled';
+
+async function initializeApiCallCounts() {
+    try {
+        const odsayDevCount = await fetchApiCallCount(DEV_SERVER, COUNT_URL + ODSAY);
+        const googleDevCount = await fetchApiCallCount(DEV_SERVER, COUNT_URL + GOOGLE);
+        const odsayProdCount = await fetchApiCallCount(PROD_SERVER, COUNT_URL + ODSAY);
+        const googleProdCount = await fetchApiCallCount(PROD_SERVER, COUNT_URL + GOOGLE);
+        const odsayTotalCount = (typeof odsayDevCount !== 'number' || typeof odsayProdCount !== 'number' ? odsayDevCount : odsayDevCount + odsayProdCount);
+        const googleTotalCount = (typeof googleDevCount !== 'number' || typeof googleProdCount !== 'number' ? googleDevCount : googleDevCount + googleProdCount);
+
+        document.querySelector('#countTable').innerHTML = `
+            <tr>
+                <td>Dev</td>
+                <td>${odsayDevCount}</td>
+                <td>${googleDevCount}</td>
+            </tr>
+            <tr>
+                <td>Prod</td>
+                <td>${odsayProdCount}</td>
+                <td>${googleProdCount}</td>
+            </tr>
+            <tr>
+                <td>Total</td>
+                <td>${odsayTotalCount}</td>
+                <td>${googleTotalCount}</td>
+            </tr>
+        `;
+    } catch (error) {
+        console.error('Error fetching api call count:', error);
+    }
+}
+
+async function fetchApiCallCount(url, endpoint) {
+    try {
+        const response = await fetch(url + endpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        const data = await response.json();
+        return data.count;
+    } catch (error) {
+        console.warn(`Error fetching ${endpoint} from ${url}:`, error);
+        return '-';
+    }
+}
+
+async function initializeApiCallEnabledButtons() {
+    const configs = [
+        {id: 'odsayDevToggleButton', server: DEV_SERVER, clientType: ODSAY},
+        {id: 'googleDevToggleButton', server: DEV_SERVER, clientType: GOOGLE},
+        {id: 'odsayProdToggleButton', server: PROD_SERVER, clientType: ODSAY},
+        {id: 'googleProdToggleButton', server: PROD_SERVER, clientType: GOOGLE}
+    ];
+
+    for (const config of configs) {
+        const button = document.getElementById(config.id);
+        await fetchApiCallEnabled(button, config.server, config.clientType);
+        button.addEventListener('click', (event) => handleButtonClick(event.target, config.server, config.clientType));
+    }
+}
+
+async function handleButtonClick(button, server, clientType) {
+    try {
+        const toggleResponse = await fetch(server + TOGGLE_URL + clientType, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        if (!toggleResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        await fetchApiCallEnabled(button, server, clientType);
+    } catch (error) {
+        console.error('Error toggling enabled button:', error);
+    }
+}
+
+async function fetchApiCallEnabled(button, server, clientType) {
+    try {
+        const response = await fetch(server + ENABLED_URL + clientType);
+        const data = await response.json();
+        updateButton(button, data.enabled);
+    } catch (error) {
+        console.warn('Error fetching api call enabled:', error);
+        button.textContent = 'unknown';
+        button.className = 'unknown-button';
+    }
+}
+
+function updateButton(button, enabled) {
+    button.textContent = enabled ? 'enabled' : 'disabled';
+    button.className = enabled ? 'blue-button' : 'red-button';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    initializeApiCallCounts();
+    initializeApiCallEnabledButtons();
+});
