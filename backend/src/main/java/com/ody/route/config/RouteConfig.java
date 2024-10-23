0db68a57fe72a8556884a919d73c386e0acbcfb2
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2024-10-23 11:06:05 +0900

    refactor: RouteClient 생성 로직 통일 (#781)
    * refactor: RouteClientProperties 추가 및 RouteClient 생성 방식 통일
    
    * test: mockServer reset
    
    * test: 불필요한 AfterEach 삭제
    
    * style: 개행 제거

--- backend/src/main/java/com/ody/route/config/RouteConfig.java
+++ backend/src/main/java/com/ody/route/config/RouteConfig.java
@@ -5,51 +5,52 @@ import com.ody.route.service.OdsayRouteClient;
 import com.ody.route.service.RouteClient;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
-import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Profile("!test")
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(RouteProperties.class)
+@EnableConfigurationProperties(RouteClientProperties.class)
 public class RouteConfig {
 
-    private final RouteProperties routeProperties;
+    private static final Duration DEFAULT_CONNECTION_TIMEOUT = Duration.ofSeconds(60);
+    private static final Duration DEFAULT_READ_TIMEOUT = Duration.ofSeconds(30);
+
+    private final RouteClientProperties properties;
 
     @Bean
     @Order(1)
-    public RouteClient odysayRouteClient(RestClient.Builder routeRestClientBuilder) {
-        return new OdsayRouteClient(routeProperties, routeRestClientBuilder);
+    public RouteClient odysayRouteClient() {
+        RouteClientProperty property = properties.getProperty("odsay");
+        return new OdsayRouteClient(property, builder());
     }
 
     @Bean
     @Order(2)
-    public RouteClient googleRouteClient(
-            RestClient.Builder routeRestClientBuilder,
-            @Value("${google.maps.api-key}") String googleApiKey
-    ) {
-        return new GoogleRouteClient(routeRestClientBuilder, googleApiKey);
+    public RouteClient googleRouteClient() {
+        RouteClientProperty property = properties.getProperty("google");
+        return new GoogleRouteClient(property, builder());
     }
 
     @Bean
-    public RestClientCustomizer routeRestClientCustomizer() {
-        return builder -> builder.requestFactory(clientHttpRequestFactory())
-                .build();
+    public RestClient.Builder builder() {
+        return RestClient.builder()
+                .requestFactory(new BufferingClientHttpRequestFactory(clientHttpRequestFactory()));
     }
 
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
-                .withConnectTimeout(Duration.ofSeconds(60))
-                .withReadTimeout(Duration.ofSeconds(30)); //TODO: timeout 처리 로직 구현
+                .withConnectTimeout(DEFAULT_CONNECTION_TIMEOUT)
+                .withReadTimeout(DEFAULT_READ_TIMEOUT);
         return ClientHttpRequestFactories.get(settings);
     }
 }
