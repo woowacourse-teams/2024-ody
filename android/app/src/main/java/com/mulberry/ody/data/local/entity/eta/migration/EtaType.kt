1341f430dd1f99fe4bff822a397e5428018d721c
Hyemin Kim <kimhm0728@naver.com>
2024-10-28 00:04:34 +0900

    refactor: Room Entity 마이그레이션 코드 리팩터링 (#850)
    * refactor: database 마이그레이션 코드 함수 분리
    
    * refactor: database 마이그레이션 코드 함수 분리
    
    * refactor: 의존성 주입 코드 제거
    
    * style: ktLint 적용
    
    * refactor: Migration 객체의 매개변수로 start, end Version 받도록 수정
    
    * refactor: 함수명 fromString -> fromJson 수정
    
    * fix: 마이그레이션 버전 변경

---
+++ android/app/src/main/java/com/mulberry/ody/data/local/entity/eta/migration/EtaType.kt
@@ -0,0 +1,9 @@
+package com.mulberry.ody.data.local.entity.eta.migration
+
+enum class EtaType {
+    LATE_WARNING,
+    ARRIVAL_SOON,
+    ARRIVED,
+    LATE,
+    MISSING,
+}
--- android/app/src/main/java/com/mulberry/ody/domain/model/MateEta2.kt
+++
@@ -1,8 +0,0 @@
-package com.mulberry.ody.domain.model
-
-data class MateEta2(
-    val mateId: Long,
-    val nickname: String,
-    val etaType: EtaType2,
-    val durationMinute: Int,
-)
