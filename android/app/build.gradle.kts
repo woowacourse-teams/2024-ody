5c9a02a2401860b861b72b0b945406caef8e29b5
aprilgom <aprilgom@users.noreply.github.com>
2024-10-31 02:46:33 +0900

    chore: 테스트용 버전 번호 변경 (#858)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.mulberry.ody"
         minSdk = 26
         targetSdk = 34
-        versionCode = 21
+        versionCode = 22
         versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
