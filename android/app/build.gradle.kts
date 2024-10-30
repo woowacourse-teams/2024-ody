ec541d7f9a6210fa69f9a501a1f5b5a5e0f56e72
kimhyemin <kimhm0728@naver.com>
2024-10-31 01:21:47 +0900

    config: versionCode, versionName 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.mulberry.ody"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.1.1"
+        versionCode = 21
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_DEV_URL", properties["BASE_DEV_URL"].toString())
