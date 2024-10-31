49709c41f95fd96f74c198ce4cc9272f459f2e46
aprilgom <aprilgom@users.noreply.github.com>
2024-10-31 17:25:38 +0900

    chore: cd 테스트용 버전 넘버 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.mulberry.ody"
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
+        versionCode = 30
         versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
