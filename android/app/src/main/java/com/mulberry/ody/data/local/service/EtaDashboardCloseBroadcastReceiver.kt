25e0cfdff8e2699db4119a6abd1772a969dd6e51
kimhyemin <kimhm0728@naver.com>
2024-11-10 15:14:29 +0900

    refactor: repository 가져오는 방식 수정

--- android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboardCloseBroadcastReceiver.kt
+++ android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboardCloseBroadcastReceiver.kt
@@ -6,14 +6,15 @@ import android.content.Intent
 import com.mulberry.ody.data.local.service.EtaDashboardService.Companion.MEETING_ID_DEFAULT_VALUE
 import com.mulberry.ody.data.local.service.EtaDashboardService.Companion.MEETING_ID_KEY
 import com.mulberry.ody.domain.repository.ody.MatesEtaRepository
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class EtaDashboardCloseBroadcastReceiver : BroadcastReceiver() {
-    @Inject
-    lateinit var matesEtaRepository: MatesEtaRepository
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
     override fun onReceive(
@@ -23,11 +24,30 @@ class EtaDashboardCloseBroadcastReceiver : BroadcastReceiver() {
         val meetingId = intent.getLongExtra(MEETING_ID_KEY, MEETING_ID_DEFAULT_VALUE)
         if (meetingId == MEETING_ID_DEFAULT_VALUE) return
 
+        val matesEtaRepository =
+            EtaDashboardCloseBroadcastReceiverEntryPoint.matesEtaRepository(context)
         coroutineScope.launch {
             matesEtaRepository.deleteEtaReservation(meetingId)
         }
 
         val serviceIntent = EtaDashboardService.getIntent(context, meetingId, isOpen = false)
         context.startForegroundService(serviceIntent)
+    }
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    private interface EtaDashboardCloseBroadcastReceiverEntryPoint {
+        fun matesEtaRepository(): MatesEtaRepository
+
+        companion object {
+            fun matesEtaRepository(context: Context): MatesEtaRepository {
+                return EntryPointAccessors
+                    .fromApplication(
+                        context,
+                        EtaDashboardCloseBroadcastReceiverEntryPoint::class.java,
+                    )
+                    .matesEtaRepository()
+            }
+        }
     }
 }
