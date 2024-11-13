3993eef96a055b4ef010befdea721b592d004cf1
H <dmsgktn0208@gmail.com>
2024-11-14 01:40:55 +0900

    test: Redis 테스트 컨테이너 설정 추가

--- backend/src/main/java/com/ody/common/redis/RedisConfig.java
+++ backend/src/main/java/com/ody/common/redis/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,6 +17,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile({"local", "dev", "prod"})
 @Configuration
 @EnableCaching
 public class RedisConfig {
---
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -0,0 +1,21 @@
+package com.ody.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@TestConfiguration
+public class RedisTestContainersConfig {
+
+    private static final int REDIS_PORT = 6379;
+
+    @Container
+    private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
+            .withExposedPorts(REDIS_PORT);
+
+    static {
+        redisContainer.start();
+    }
+}
