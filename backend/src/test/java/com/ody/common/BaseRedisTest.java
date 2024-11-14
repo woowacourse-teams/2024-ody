5619da10a4e767a0ea2c6ea55da392b46ec7a9e9
H <dmsgktn0208@gmail.com>
2024-11-14 16:07:23 +0900

    test: 레디스 테스트를 공통화한 추상 클래스 추가

---
+++ backend/src/test/java/com/ody/common/BaseRedisTest.java
@@ -0,0 +1,30 @@
+package com.ody.common;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.ody.common.redis.CustomRedisTemplate;
+import com.ody.notification.config.FcmConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+@Import({RedisTestContainersConfig.class, TestRouteConfig.class})
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public abstract class BaseRedisTest {
+
+    @Autowired
+    protected CustomRedisTemplate redisTemplate;
+
+    @MockBean
+    private FcmConfig fcmConfig;
+
+    @MockBean
+    private FirebaseMessaging firebaseMessaging;
+
+    @BeforeEach
+    void setUp() {
+        redisTemplate.getConnectionFactory().getConnection().serverCommands().flushAll();
+    }
+}
