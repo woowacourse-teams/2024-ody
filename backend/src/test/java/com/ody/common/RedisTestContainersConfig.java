e98a9fc180656c4b7bae4ccb1fadc94a93ffe09d
H <dmsgktn0208@gmail.com>
2024-11-20 22:18:03 +0900

    test: 레디스 연결 설정 오류 수정

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,31 +1,31 @@
 package com.ody.common;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @TestConfiguration
+@Testcontainers
 public class RedisTestContainersConfig {
-
     private static final int REDIS_PORT = 6379;
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
             .withExposedPorts(REDIS_PORT);
 
-    @DynamicPropertySource
-    static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.host", redisContainer::getHost);
-        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(REDIS_PORT));
+    static {
+        redisContainer.start();
     }
 
-    @BeforeAll
-    static void setUp() {
-        redisContainer.start();
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                redisContainer.getHost(),
+                redisContainer.getMappedPort(REDIS_PORT)
+        );
     }
 }
