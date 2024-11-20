9bb7b8bec42975d54a7653235b5e50b7e22a13a1
H <dmsgktn0208@gmail.com>
2024-11-20 20:50:53 +0900

    test: 레디스 테스트 컨테이너 로깅 추가

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -2,6 +2,9 @@ package com.ody.common;
 
 import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -21,7 +24,20 @@ public class RedisTestContainersConfig {
 
     static {
         redisContainer.start();
+
+        System.out.println("레디스 컨테이너 시작");
+        System.out.println("호스트 : " + redisContainer.getHost());
+        System.out.println("포트 : " + redisContainer.getMappedPort(REDIS_PORT));
+        
         System.setProperty("spring.data.redis.host", redisContainer.getHost());
         System.setProperty("spring.data.redis.port", String.valueOf(redisContainer.getMappedPort(REDIS_PORT)));
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                redisContainer.getHost(),
+                redisContainer.getMappedPort(REDIS_PORT)
+        );
     }
 }
