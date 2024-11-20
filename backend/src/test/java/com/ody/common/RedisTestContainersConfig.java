6e507c7e2ece9595a51ff42a8ecab0ea3fc22ac7
H <dmsgktn0208@gmail.com>
2024-11-20 21:13:15 +0900

    test: DynamicPropertySource로 레디스 테스트 컨테이너 연결 설정

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,45 +1,31 @@
 package com.ody.common;
 
-import java.time.Duration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers(disabledWithoutDocker = true)
+@Testcontainers
 @TestConfiguration
 public class RedisTestContainersConfig {
-    private static final Logger log = LoggerFactory.getLogger(RedisTestContainersConfig.class);
+
     private static final int REDIS_PORT = 6379;
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
-            .withExposedPorts(REDIS_PORT)
-            .waitingFor(Wait.forListeningPort())
-            .withStartupTimeout(Duration.ofSeconds(30));
+            .withExposedPorts(REDIS_PORT);
 
-    static {
-        redisContainer.start();
-
-        System.out.println("레디스 컨테이너 시작");
-        System.out.println("호스트 : " + redisContainer.getHost());
-        System.out.println("포트 : " + redisContainer.getMappedPort(REDIS_PORT));
-
-        System.setProperty("spring.data.redis.host", redisContainer.getHost());
-        System.setProperty("spring.data.redis.port", String.valueOf(redisContainer.getMappedPort(REDIS_PORT)));
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(REDIS_PORT));
     }
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(
-                redisContainer.getHost(),
-                redisContainer.getMappedPort(REDIS_PORT)
-        );
+    @BeforeAll
+    static void setUp() {
+        redisContainer.start();
     }
 }
