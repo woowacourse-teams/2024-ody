144e307e2b2c06bf941dbbb60e6e1cae05469190
H <dmsgktn0208@gmail.com>
2024-11-20 20:30:36 +0900

    test: 레디스 테스트 컨테이너 타임아웃 추가

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,7 +1,9 @@
 package com.ody.common;
 
+import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -13,7 +15,9 @@ public class RedisTestContainersConfig {
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
-            .withExposedPorts(REDIS_PORT);
+            .withExposedPorts(REDIS_PORT)
+            .waitingFor(Wait.forListeningPort())
+            .withStartupTimeout(Duration.ofSeconds(30));
 
     static {
         redisContainer.start();
