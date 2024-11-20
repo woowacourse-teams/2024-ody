fad1cb37a7a8881680cab7d110d9cfd5566217df
H <dmsgktn0208@gmail.com>
2024-11-21 00:00:56 +0900

    chore: 레디스 테스트 설정 수정

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,10 +1,11 @@
 package com.ody.common;
 
 import java.time.Duration;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -13,7 +14,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @TestConfiguration
 @Testcontainers
 public class RedisTestContainersConfig {
+
     private static final int REDIS_PORT = 6379;
+    private static final int THREE_SECOND = 3000;
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
@@ -26,10 +29,12 @@ public class RedisTestContainersConfig {
     }
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(
-                redisContainer.getHost(),
-                redisContainer.getMappedPort(REDIS_PORT)
-        );
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisContainer.getHost() + ":" + redisContainer.getMappedPort(REDIS_PORT))
+                .setConnectTimeout(THREE_SECOND)
+                .setRetryAttempts(3);
+        return Redisson.create(config);
     }
 }
