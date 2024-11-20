682d870320ccbfa18d2cd666095b65dec1461d56
H <dmsgktn0208@gmail.com>
2024-11-20 22:37:12 +0900

    test: 레디스 연결 설정 대기 옵션 추가

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,10 +1,12 @@
 package com.ody.common;
 
+import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -15,7 +17,9 @@ public class RedisTestContainersConfig {
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
-            .withExposedPorts(REDIS_PORT);
+            .withExposedPorts(REDIS_PORT)
+            .waitingFor(Wait.forListeningPort())
+            .withStartupTimeout(Duration.ofSeconds(60));
 
     static {
         redisContainer.start();
