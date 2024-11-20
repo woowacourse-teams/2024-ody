cabb704103d01db90ef37c4d3c0b72f9a3ab8650
H <dmsgktn0208@gmail.com>
2024-11-20 21:00:27 +0900

    test: 레디스 테스트 컨테이너 disabledWithoutDocker 옵션 추가

--- backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
+++ backend/src/test/java/com/ody/common/RedisTestContainersConfig.java
@@ -1,6 +1,8 @@
 package com.ody.common;
 
 import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,10 +12,10 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 @TestConfiguration
 public class RedisTestContainersConfig {
-
+    private static final Logger log = LoggerFactory.getLogger(RedisTestContainersConfig.class);
     private static final int REDIS_PORT = 6379;
 
     @Container
@@ -28,7 +30,7 @@ public class RedisTestContainersConfig {
         System.out.println("레디스 컨테이너 시작");
         System.out.println("호스트 : " + redisContainer.getHost());
         System.out.println("포트 : " + redisContainer.getMappedPort(REDIS_PORT));
-        
+
         System.setProperty("spring.data.redis.host", redisContainer.getHost());
         System.setProperty("spring.data.redis.port", String.valueOf(redisContainer.getMappedPort(REDIS_PORT)));
     }
