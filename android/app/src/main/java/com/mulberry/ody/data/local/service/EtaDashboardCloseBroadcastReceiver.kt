223877cd32d40fbfc5eb16798ceac7bc7cd3ef92
kimhyemin <kimhm0728@naver.com>
2024-11-21 17:22:03 +0900

    refactor: matesEtaRepository 주입 방식 변경

--- android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboardCloseBroadcastReceiver.kt
+++ android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboardCloseBroadcastReceiver.kt
@@ -6,15 +6,16 @@ import android.content.Intent
 import com.mulberry.ody.data.local.service.EtaDashboardService.Companion.MEETING_ID_DEFAULT_VALUE
 import com.mulberry.ody.data.local.service.EtaDashboardService.Companion.MEETING_ID_KEY
 import com.mulberry.ody.domain.repository.ody.MatesEtaRepository
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EntryPointAccessors
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class EtaDashboardCloseBroadcastReceiver : BroadcastReceiver() {
+    @Inject
+    lateinit var matesEtaRepository: MatesEtaRepository
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
     override fun onReceive(
@@ -24,30 +25,11 @@ class EtaDashboardCloseBroadcastReceiver : BroadcastReceiver() {
         val meetingId = intent.getLongExtra(MEETING_ID_KEY, MEETING_ID_DEFAULT_VALUE)
         if (meetingId == MEETING_ID_DEFAULT_VALUE) return
 
-        val matesEtaRepository =
-            EtaDashboardCloseBroadcastReceiverEntryPoint.matesEtaRepository(context)
         coroutineScope.launch {
             matesEtaRepository.deleteEtaReservation(meetingId)
         }
 
         val serviceIntent = EtaDashboardService.getIntent(context, meetingId, isOpen = false)
         context.startForegroundService(serviceIntent)
-    }
-
-    @EntryPoint
-    @InstallIn(SingletonComponent::class)
-    interface EtaDashboardCloseBroadcastReceiverEntryPoint {
-        fun matesEtaRepository(): MatesEtaRepository
-
-        companion object {
-            fun matesEtaRepository(context: Context): MatesEtaRepository {
-                return EntryPointAccessors
-                    .fromApplication(
-                        context,
-                        EtaDashboardCloseBroadcastReceiverEntryPoint::class.java,
-                    )
-                    .matesEtaRepository()
-            }
-        }
     }
 }
--- android/app/src/main/java/com/mulberry/ody/data/local/service/ReinstallBroadcastReceiver.kt
+++ android/app/src/main/java/com/mulberry/ody/data/local/service/ReinstallBroadcastReceiver.kt
@@ -4,38 +4,24 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.mulberry.ody.domain.repository.ody.MatesEtaRepository
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EntryPointAccessors
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ReinstallBroadcastReceiver : BroadcastReceiver() {
+    @Inject
+    lateinit var matesEtaRepository: MatesEtaRepository
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
     override fun onReceive(
         context: Context,
         intent: Intent,
     ) {
-        val matesEtaRepository = ReinstallBroadcastReceiverEntryPoint.matesEtaRepository(context)
         coroutineScope.launch {
             matesEtaRepository.reserveAllEtaReservation()
-        }
-    }
-
-    @EntryPoint
-    @InstallIn(SingletonComponent::class)
-    interface ReinstallBroadcastReceiverEntryPoint {
-        fun matesEtaRepository(): MatesEtaRepository
-
-        companion object {
-            fun matesEtaRepository(context: Context): MatesEtaRepository {
-                return EntryPointAccessors
-                    .fromApplication(context, ReinstallBroadcastReceiverEntryPoint::class.java)
-                    .matesEtaRepository()
-            }
         }
     }
 }
