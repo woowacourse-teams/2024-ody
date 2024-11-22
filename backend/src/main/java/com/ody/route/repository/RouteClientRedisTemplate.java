ba2b39faa4ee6fc053ebf2e1d8aa5a39ea58da5d
H <dmsgktn0208@gmail.com>
2024-11-22 19:48:08 +0900

    refactor: redis increase() 예외 처리 추가

--- backend/src/main/java/com/ody/route/repository/RouteClientRedisTemplate.java
+++ backend/src/main/java/com/ody/route/repository/RouteClientRedisTemplate.java
@@ -1,13 +1,16 @@
 package com.ody.route.repository;
 
+import com.ody.common.exception.OdyServerErrorException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RouteClientRedisTemplate extends RedisTemplate<String, String> {
@@ -25,7 +28,10 @@ public class RouteClientRedisTemplate extends RedisTemplate<String, String> {
     public int increment(String key) {
         return Optional.ofNullable(opsForValue().increment(key))
                 .map(Long::intValue)
-                .orElse(0);
+                .orElseThrow(() -> {
+                    log.error("redis key 값({})이 존재하지 않아 증가시킬 수 없습니다.", key);
+                    return new OdyServerErrorException("서버에 장애가 발생했습니다.");
+                });
     }
 
     public int getKeyCount(String key) {
