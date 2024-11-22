b493cb88628bfce0fd39aeb61b0023bd37425af9
coli-geonwoo <coli-geonwoo@gmail.com>
2024-11-23 03:00:32 +0900

    test: 불필요한 import 문 삭제

--- backend/src/test/java/com/ody/common/exception/AsyncExceptionHandlerTest.java
+++ backend/src/test/java/com/ody/common/exception/AsyncExceptionHandlerTest.java
@@ -8,24 +8,14 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.ody.common.BaseControllerTest;
-import com.ody.common.BaseServiceTest;
-import com.ody.common.FixtureGeneratorConfig;
-import com.ody.common.TestAuthConfig;
-import com.ody.common.TestRouteConfig;
-import com.ody.notification.service.FcmEventListener;
 import com.ody.notification.service.FcmSubscriber;
 import com.ody.notification.service.event.SubscribeEvent;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.RecordApplicationEvents;
 
 class AsyncExceptionHandlerTest extends BaseControllerTest {
 
