5549d71617170c9e95ae713e05400266c56c5ce4
kimhyemin <kimhm0728@naver.com>
2025-01-04 14:26:00 +0900

    style: ktLint 적용

--- android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboard.kt
+++ android/app/src/main/java/com/mulberry/ody/data/local/service/EtaDashboard.kt
@@ -12,34 +12,34 @@ import java.time.LocalDateTime
 import javax.inject.Inject
 
 class EtaDashboard
-@Inject
-constructor(
-    val context: Context,
-    val odyDatastore: OdyDatastore,
-) {
-    fun open(
-        meetingId: Long,
-        meetingTime: LocalDateTime,
+    @Inject
+    constructor(
+        val context: Context,
+        val odyDatastore: OdyDatastore,
     ) {
-        CoroutineScope(Dispatchers.Default).launch {
-            if (!isLoggedIn()) {
-                return@launch
-            }
+        fun open(
+            meetingId: Long,
+            meetingTime: LocalDateTime,
+        ) {
+            CoroutineScope(Dispatchers.Default).launch {
+                if (!isLoggedIn()) {
+                    return@launch
+                }
 
-            startEtaDashboardService(meetingId, meetingTime)
+                startEtaDashboardService(meetingId, meetingTime)
+            }
+        }
+
+        private suspend fun isLoggedIn(): Boolean {
+            return coroutineScope { odyDatastore.getAuthToken().first().isSuccess }
+        }
+
+        private fun startEtaDashboardService(
+            meetingId: Long,
+            meetingTime: LocalDateTime,
+        ) {
+            val meetingTimeMills = meetingTime.toMilliSeconds()
+            val serviceIntent = EtaDashboardService.getIntent(context, meetingId, meetingTimeMills, isOpen = true)
+            context.startForegroundService(serviceIntent)
         }
     }
-
-    private suspend fun isLoggedIn(): Boolean {
-        return coroutineScope { odyDatastore.getAuthToken().first().isSuccess }
-    }
-
-    private fun startEtaDashboardService(
-        meetingId: Long,
-        meetingTime: LocalDateTime,
-    ) {
-        val meetingTimeMills = meetingTime.toMilliSeconds()
-        val serviceIntent = EtaDashboardService.getIntent(context, meetingId, meetingTimeMills, isOpen = true)
-        context.startForegroundService(serviceIntent)
-    }
-}
