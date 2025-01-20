8b09e38ea64bc5f451c4f621b3da3d7a89428175
kimhyemin <kimhm0728@naver.com>
2025-01-20 14:46:43 +0900

    feat: meetingDetailUiModel 화면에 띄우는 로직 추가

--- android/app/src/main/java/com/mulberry/ody/presentation/room/detail/DetailMeetingBindingAdapter.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/room/detail/DetailMeetingBindingAdapter.kt
@@ -1,9 +1,12 @@
 package com.mulberry.ody.presentation.room.detail
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.mulberry.ody.R
 import com.mulberry.ody.presentation.common.getPointOnScreen
 import com.mulberry.ody.presentation.room.detail.listener.DepartureTimeGuideListener
+import com.mulberry.ody.presentation.room.detail.model.MeetingDetailUiModel
 
 @BindingAdapter("onClickDepartureTimeGuide")
 fun ImageView.setOnClickDepartureTimeGuide(missingToolTipListener: DepartureTimeGuideListener) {
@@ -12,3 +15,13 @@ fun ImageView.setOnClickDepartureTimeGuide(missingToolTipListener: DepartureTime
         missingToolTipListener.toggleDepartureTimeGuide(point)
     }
 }
+
+@BindingAdapter("departureTime")
+fun TextView.setDepartureTimeText(detailMeetingUiModel: MeetingDetailUiModel) {
+    text = context.getString(
+        R.string.detail_meeting_departure_time_content,
+        detailMeetingUiModel.departureTime,
+        detailMeetingUiModel.routeTime,
+    )
+}
+
