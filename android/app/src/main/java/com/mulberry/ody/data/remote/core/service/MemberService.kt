245f79d486b1d2c2a3fdfacb1085c2489690bcd5
kimhyemin <kimhm0728@naver.com>
2025-02-02 16:31:13 +0900

    chore: 사용하지 않는 파일 제거

--- android/app/src/main/java/com/mulberry/ody/data/remote/core/service/MemberService.kt
+++
@@ -1,9 +0,0 @@
-package com.mulberry.ody.data.remote.core.service
-
-import com.mulberry.ody.domain.apiresult.ApiResult
-import retrofit2.http.DELETE
-
-interface MemberService {
-    @DELETE("/members")
-    suspend fun deleteMember(): ApiResult<Unit>
-}
