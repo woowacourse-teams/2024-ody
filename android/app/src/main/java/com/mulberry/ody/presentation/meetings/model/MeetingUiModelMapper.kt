f78d72511fba2e72fa162b912ea38acf89ebd045
Hyemin Kim <kimhm0728@naver.com>
2025-02-04 21:54:53 +0900

    design: 도착지까지 걸리는 시간 문구 수정 (#972)
    * design: 메인 화면 걸리는 시간 문구 수정
    
    * design: X시 X분 -> X시간 X분으로 수정

--- android/app/src/main/java/com/mulberry/ody/presentation/meetings/model/MeetingUiModelMapper.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/meetings/model/MeetingUiModelMapper.kt
@@ -1,16 +1,17 @@
 package com.mulberry.ody.presentation.meetings.model
 
 import com.mulberry.ody.domain.model.MeetingCatalog
+import com.mulberry.ody.presentation.common.toDurationTimeMessage
 import java.time.LocalDateTime
 
 fun MeetingCatalog.toMeetingCatalogUiModel(): MeetingUiModel {
     val now = LocalDateTime.now()
     val inDuration = datetime.isBefore(now.plusMinutes(30))
     return MeetingUiModel(
+        id = id,
         name = name,
         datetime = datetime,
-        durationMinutes = durationMinutes.toString(),
-        id = id,
+        durationMinutes = durationMinutes.toInt().toDurationTimeMessage(),
         originAddress = originAddress,
         targetAddress = targetAddress,
         isEnabled = inDuration,
