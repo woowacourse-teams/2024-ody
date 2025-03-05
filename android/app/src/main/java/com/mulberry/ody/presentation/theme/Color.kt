bcca7bc742fe783b5991e3af8f5c2aaaabbd1be0
kimhyemin <kimhm0728@naver.com>
2025-03-05 22:55:09 +0900

    design: 컴포즈 테마 구현

---
+++ android/app/src/main/java/com/mulberry/ody/presentation/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.mulberry.ody.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cream = Color(0xFFF9F9F8)
+val Purple300 = Color(0xFFB8A7FF)
+val Purple800 = Color(0xFF5C47AE)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Gray100 = Color(0xFFF9F9F8)
+val Gray200 = Color(0xFFF2F2F6)
+val Gray300 = Color(0xFFE4E4EA)
+val Gray350 = Color(0xFFD6D6D6)
+val Gray400 = Color(0xFFB1B1BB)
+val Gray500 = Color(0xFF8E8E93)
+val Gray600 = Color(0xFF636366)
+val Gray700 = Color(0xFF48484A)
+val Gray800 = Color(0xFF3A3A3C)
+val Gray900 = Color(0xFF1C1C1E)
+
+val Blue = Color(0xFF5B89FF)
+val Red = Color(0xFFFF5B5B)
+val Green = Color(0xFF70BE69)
+val Yellow = Color(0xFFF0C65B)
