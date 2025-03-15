0ef730f7e70f1905e9aeff7008d1ea1224c80862
kimhyemin <kimhm0728@naver.com>
2025-03-15 16:24:32 +0900

    design: 툴바 구현

--- android/app/src/main/java/com/mulberry/ody/presentation/feature/meetings/MeetingsScreen.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/meetings/MeetingsScreen.kt
@@ -4,21 +4,36 @@ import android.Manifest
 import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
 import com.mulberry.ody.R
+import com.mulberry.ody.presentation.common.OnLifecycleEvent
+import com.mulberry.ody.presentation.theme.OdyTheme
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MeetingsScreen(
     viewModel: MeetingsViewModel = hiltViewModel(),
@@ -35,6 +50,32 @@ fun MeetingsScreen(
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
+        modifier = Modifier.background(OdyTheme.colors.primary),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = OdyTheme.colors.primary,
+                ),
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = OdyTheme.typography.pretendardBold28.copy(color = OdyTheme.colors.quinary),
+                    )
+                },
+                actions = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_setting),
+                        modifier = Modifier
+                            .clickable { viewModel.onClickSetting() }
+                            .padding(end = 18.dp),
+                        tint = OdyTheme.colors.tertiary,
+                        contentDescription = null,
+                    )
+                },
+            )
+        },
     ) { innerPadding ->
         Text(text = "안녕", modifier = Modifier.padding(innerPadding))
     }
