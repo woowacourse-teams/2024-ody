f91e0e20b0b75323083cd0ccee0582af590652e7
kimhyemin <kimhm0728@naver.com>
2025-03-17 22:53:35 +0900

    chore: 사용하지 않는 파일 제거

--- android/app/src/main/java/com/mulberry/ody/presentation/feature/meetings/listener/MeetingsItemListener.kt
+++
@@ -1,7 +0,0 @@
-package com.mulberry.ody.presentation.feature.meetings.listener
-
-interface MeetingsItemListener {
-    fun navigateToNotificationLog(meetingId: Long)
-
-    fun navigateToEtaDashboard(meetingId: Long)
-}
