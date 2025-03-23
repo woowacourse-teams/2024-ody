985f107fe7f3127c567fb369aaf1875fb7117059
coli <kkwoo001021@naver.com>
2025-03-24 00:20:10 +0900

    feat: meetingLog 엔티티 추가

---
+++ backend/src/main/java/com/ody/meetinglog/domain/MeetingLog.java
@@ -0,0 +1,40 @@
+package com.ody.meetinglog.domain;
+
+import com.ody.common.domain.BaseEntity;
+import com.ody.mate.domain.Mate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class MeetingLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @JoinColumn(name = "mate_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Mate mate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private MeetingLogType type;
+
+    public MeetingLog(Mate mate, MeetingLogType type) {
+        this(null, mate, type);
+    }
+}
