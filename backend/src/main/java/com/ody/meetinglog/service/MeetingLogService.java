3250a13790a6ff13219f6509889839d3538b8ff0
coli <kkwoo001021@naver.com>
2025-03-24 02:30:23 +0900

    style: transactional 컨벤션 준수

--- backend/src/main/java/com/ody/meetinglog/service/MeetingLogService.java
+++ backend/src/main/java/com/ody/meetinglog/service/MeetingLogService.java
@@ -6,13 +6,16 @@ import com.ody.notification.dto.response.NotiLogFindResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MeetingLogService {
 
     private final MeetingLogRepository meetingLogRepository;
 
+    @Transactional
     public MeetingLog save(MeetingLog meetingLog) {
         return meetingLogRepository.save(meetingLog);
     }
