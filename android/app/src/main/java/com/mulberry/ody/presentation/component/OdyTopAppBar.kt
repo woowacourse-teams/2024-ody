fe6c04087600bbb1a33391a4b12592758cf063f5
kimhyemin <kimhm0728@naver.com>
2025-03-26 15:02:56 +0900

    test: OdyTopAppBar 프리뷰 추가

--- android/app/src/main/java/com/mulberry/ody/presentation/component/OdyTopAppBar.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/component/OdyTopAppBar.kt
@@ -2,12 +2,17 @@ package com.mulberry.ody.presentation.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.mulberry.ody.R
 import com.mulberry.ody.presentation.theme.OdyTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,4 +39,30 @@ fun OdyTopAppBar(
         navigationIcon = { navigationIcon?.let { it() } },
         actions = { actions?.let { it() } },
     )
+}
+
+@Composable
+@Preview
+private fun OdyTopAppBarPreview() {
+    OdyTheme {
+        OdyTopAppBar(
+            title = "오디",
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = null,
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_setting),
+                        contentDescription = null,
+                    )
+                }
+            },
+        )
+    }
 }
