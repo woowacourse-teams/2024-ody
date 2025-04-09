cadd0a37a72271f0633964db3fb2fd1b55e06611
mzeong <mjeong@sookmyung.ac.kr>
2025-04-09 23:45:57 +0900

    fix: 애플 로그인 AppleRefreshToken 널 값 에러 해결

--- backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
+++ backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
@@ -1,10 +1,22 @@
 package com.ody.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AppleAuthValidationResponse(
+
+        @JsonProperty("access_token")
         String accessToken,
+
+        @JsonProperty("token_type")
         String tokenType,
-        String expiresIn,
+
+        @JsonProperty("expires_in")
+        int expiresIn,
+
+        @JsonProperty("refresh_token")
         String refreshToken,
+
+        @JsonProperty("id_token")
         String idToken
 ) {
 
