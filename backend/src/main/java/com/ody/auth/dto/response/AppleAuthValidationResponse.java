87013c107e752799269b492bd2f89224dec03b1f
mzeong <mjeong@sookmyung.ac.kr>
2025-04-10 20:20:16 +0900

    refactor: JsonNaming 사용해 중복 코드 제거

--- backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
+++ backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
@@ -1,22 +1,16 @@
 package com.ody.auth.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AppleAuthValidationResponse(
 
-        @JsonProperty("access_token")
         String accessToken,
-
-        @JsonProperty("token_type")
         String tokenType,
-
-        @JsonProperty("expires_in")
         int expiresIn,
-
-        @JsonProperty("refresh_token")
         String refreshToken,
-
-        @JsonProperty("id_token")
         String idToken
 ) {
 
