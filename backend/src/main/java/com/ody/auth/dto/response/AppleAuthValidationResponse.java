b5d3480752ad03207094d09051580755a2f9b096
mzeong <mzeongdev@gmail.com>
2025-04-10 20:20:32 +0900

    fix: 애플 로그인 AppleRefreshToken 널 값 에러 해결 (#995)
    * fix: 애플 로그인 AppleRefreshToken 널 값 에러 해결
    
    * refactor: JsonNaming 사용해 중복 코드 제거

--- backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
+++ backend/src/main/java/com/ody/auth/dto/response/AppleAuthValidationResponse.java
@@ -1,9 +1,15 @@
 package com.ody.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AppleAuthValidationResponse(
+
         String accessToken,
         String tokenType,
-        String expiresIn,
+        int expiresIn,
         String refreshToken,
         String idToken
 ) {
