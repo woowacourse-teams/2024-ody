71bfcab2abefe9690992412f331efc29accb1d2b
kimhyemin <kimhm0728@naver.com>
2025-05-29 21:19:43 +0900

    chore: 캡쳐 로직 제거

--- android/app/src/main/java/com/mulberry/ody/presentation/feature/room/etadashboard/EtaDashBoardScreen.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/room/etadashboard/EtaDashBoardScreen.kt
@@ -37,13 +37,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.draw
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -129,20 +124,7 @@ fun EtaDashboardContent(
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
-        modifier =
-            modifier
-                .drawWithCache {
-                    val width = size.width.toInt()
-                    val height = size.height.toInt()
-                    onDrawWithContent {
-                        val pictureCanvas = Canvas(picture.beginRecording(width, height))
-                        draw(this, layoutDirection, pictureCanvas, size) {
-                            this@onDrawWithContent.drawContent()
-                        }
-                        picture.endRecording()
-                        drawIntoCanvas { canvas -> canvas.nativeCanvas.drawPicture(picture) }
-                    }
-                },
+        modifier = modifier,
         contentPadding = PaddingValues(vertical = 32.dp),
         verticalArrangement = Arrangement.spacedBy(42.dp),
     ) {
