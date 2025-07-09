79497af523f460f1c7f5a05bbec243b6568e0aa2
kimhyemin <kimhm0728@naver.com>
2025-07-10 02:31:56 +0900

    refactor: entity serialization으로 변경

--- android/app/src/main/java/com/mulberry/ody/domain/model/EtaStatus.kt
+++ android/app/src/main/java/com/mulberry/ody/domain/model/EtaStatus.kt
@@ -1,13 +1,21 @@
 package com.mulberry.ody.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface EtaStatus {
+    @Serializable
     data object Arrived : EtaStatus
 
+    @Serializable
     data class ArrivalSoon(val durationMinutes: Int) : EtaStatus
 
+    @Serializable
     data class LateWarning(val durationMinutes: Int) : EtaStatus
 
+    @Serializable
     data class Late(val durationMinutes: Int) : EtaStatus
 
+    @Serializable
     data object Missing : EtaStatus
 }
--- android/app/src/main/java/com/mulberry/ody/domain/model/MateEta.kt
+++ android/app/src/main/java/com/mulberry/ody/domain/model/MateEta.kt
@@ -1,5 +1,8 @@
 package com.mulberry.ody.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MateEta(
     val mateId: Long,
     val nickname: String,
