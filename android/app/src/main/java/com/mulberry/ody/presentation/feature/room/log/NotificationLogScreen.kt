f77c28a1e5631d00d372ee4c68082ddf6f2dc9ca
kimhyemin <kimhm0728@naver.com>
2025-07-15 22:30:13 +0900

    feat: NotificationLogScreen 컴포저블 생성

---
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/room/log/NotificationLogScreen.kt
@@ -0,0 +1,10 @@
+package com.mulberry.ody.presentation.feature.room.log
+
+import androidx.compose.runtime.Composable
+import com.mulberry.ody.presentation.feature.room.MeetingRoomViewModel
+
+@Composable
+fun NotificationLogScreen(
+    viewModel: MeetingRoomViewModel,
+) {
+}
