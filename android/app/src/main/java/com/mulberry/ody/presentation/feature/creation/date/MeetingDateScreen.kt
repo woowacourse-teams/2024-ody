8025d68464cd920e00a77d03130fe11d82938e78
kimhyemin <kimhm0728@naver.com>
2025-07-20 22:56:56 +0900

    design: 이름, 날짜 입력 화면 ui 구현

---
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/creation/date/MeetingDateScreen.kt
@@ -0,0 +1,88 @@
+package com.mulberry.ody.presentation.feature.creation.date
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mulberry.ody.R
+import com.mulberry.ody.presentation.component.OdyTextField
+import com.mulberry.ody.presentation.feature.creation.MeetingCreationViewModel
+import com.mulberry.ody.presentation.theme.OdyTheme
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MeetingDateScreen() {
+    val datePickerState = rememberDatePickerState(
+        initialDisplayMode = DisplayMode.Picker,
+        initialSelectedDateMillis = System.currentTimeMillis(),
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return utcTimeMillis > System.currentTimeMillis()
+            }
+        }
+    )
+    val selectedDate = datePickerState.selectedDateMillis
+
+        Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text =
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.secondary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_date_question_front))
+                }
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.quinary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_date_question_back))
+                }
+            },
+            style = OdyTheme.typography.pretendardBold24,
+            modifier = Modifier.padding(top = 52.dp, bottom = 32.dp),
+        )
+        DatePicker(
+            state = datePickerState,
+            showModeToggle = false,
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MeetingDateScreenPreview() {
+    OdyTheme {
+        MeetingDateScreen()
+    }
+}
---
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/creation/name/MeetingNameScreen.kt
@@ -0,0 +1,90 @@
+package com.mulberry.ody.presentation.feature.creation.name
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mulberry.ody.R
+import com.mulberry.ody.presentation.component.OdyTextField
+import com.mulberry.ody.presentation.feature.creation.MeetingCreationViewModel
+import com.mulberry.ody.presentation.theme.OdyTheme
+
+@Composable
+fun MeetingNameScreen() {
+    var meetingName by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text =
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.quinary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_name_question_front))
+                }
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.secondary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_name_question_middle))
+                }
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.quinary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_name_question_back))
+                }
+            },
+            style = OdyTheme.typography.pretendardBold24,
+            modifier = Modifier.padding(top = 52.dp, bottom = 32.dp),
+        )
+        OdyTextField(
+            value = meetingName,
+            onValueChange = { meetingName = it },
+            placeholder = stringResource(id = R.string.meeting_name_question_hint),
+            trailingIcon = {
+                Text(
+                    text = stringResource(
+                        id = R.string.meeting_name_length,
+                        meetingName.length,
+                        MeetingCreationViewModel.MEETING_NAME_MAX_LENGTH,
+                    ),
+                    style = OdyTheme.typography.pretendardMedium16.copy(color = OdyTheme.colors.senary),
+                )
+            },
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MeetingNameScreenPreview() {
+    OdyTheme {
+        MeetingNameScreen()
+    }
+}
