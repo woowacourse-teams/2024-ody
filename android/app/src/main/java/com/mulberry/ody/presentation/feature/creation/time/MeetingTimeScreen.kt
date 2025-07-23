ce7a27cd3d7177667510753f3bf6c0f41c60bb0d
kimhyemin <kimhm0728@naver.com>
2025-07-24 01:56:27 +0900

    design: 약속 시간 입력 화면 ui 구현

---
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/creation/time/MeetingTimeScreen.kt
@@ -0,0 +1,89 @@
+package com.mulberry.ody.presentation.feature.creation.time
+
+import android.inputmethodservice.Keyboard
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mulberry.ody.R
+import com.mulberry.ody.presentation.component.OdyNumberPicker
+import com.mulberry.ody.presentation.theme.OdyTheme
+
+@Composable
+fun MeetingTimeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text =
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.secondary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_time_question_front))
+                }
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = OdyTheme.colors.quinary
+                    )
+                ) {
+                    append(stringResource(id = R.string.meeting_time_question_back))
+                }
+            },
+            style = OdyTheme.typography.pretendardBold24,
+            modifier = Modifier.padding(top = 52.dp, bottom = 32.dp),
+        )
+        Spacer(modifier = Modifier.height(74.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(32.dp),
+        ) {
+            OdyNumberPicker(
+                value = 0,
+                range = 0..24,
+                onValueChange = {},
+            )
+            Text(
+                text = stringResource(id = R.string.meeting_time_separator),
+                style = OdyTheme.typography.pretendardBold28.copy(color = OdyTheme.colors.tertiary)
+            )
+            OdyNumberPicker(
+                value = 0,
+                range = 0..59,
+                onValueChange = {},
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MeetingTimeScreenPreview() {
+    OdyTheme {
+        MeetingTimeScreen()
+    }
+}
