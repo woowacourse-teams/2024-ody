4964075b29265088ecfaa87538eb539b5e91fdc9
kimhyemin <kimhm0728@naver.com>
2025-08-01 21:12:34 +0900

    feat: 다음 버튼 누르면 키보드 사라지는 기능 구현

--- android/app/src/main/java/com/mulberry/ody/presentation/feature/creation/date/MeetingDateScreen.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/creation/date/MeetingDateScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -34,6 +35,12 @@ fun MeetingDateScreen(
     date: LocalDate,
     onDateChanged: (LocalDate) -> Unit,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(Unit) {
+        keyboardController?.hide()
+    }
+
     val datePickerState =
         rememberDatePickerState(
             initialDisplayMode = DisplayMode.Picker,
