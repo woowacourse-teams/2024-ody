9e5d40055182a954337cad5c6c0d530f965157fc
kimhyemin <kimhm0728@naver.com>
2025-08-04 16:54:47 +0900

    refactor: response localdatetime 변환 함수 util로 분리

--- android/app/src/main/java/com/mulberry/ody/data/remote/core/entity/meeting/mapper/MeetingResponseMapper.kt
+++ android/app/src/main/java/com/mulberry/ody/data/remote/core/entity/meeting/mapper/MeetingResponseMapper.kt
@@ -1,20 +1,18 @@
 package com.mulberry.ody.data.remote.core.entity.meeting.mapper
 
 import com.mulberry.ody.data.remote.core.entity.meeting.response.DetailMeetingResponse
+import com.mulberry.ody.data.util.convertLocalDateTime
+import com.mulberry.ody.data.util.toLocalTime
 import com.mulberry.ody.domain.model.DetailMeeting
 import com.mulberry.ody.domain.model.Mate
 import com.mulberry.ody.domain.model.MeetingDateTime
 import com.mulberry.ody.domain.model.MeetingName
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 fun DetailMeetingResponse.toDetailMeeting(): DetailMeeting =
     DetailMeeting(
         id = id,
         name = MeetingName(name),
-        dateTime = convertMeetingDateTime(date, time),
+        dateTime = MeetingDateTime(dateTime = convertLocalDateTime(date, time)),
         destinationAddress = targetAddress,
         departureAddress = originAddress,
         departureTime = departureTime.toLocalTime(),
@@ -22,21 +20,3 @@ fun DetailMeetingResponse.toDetailMeeting(): DetailMeeting =
         mates = mates.map { Mate(nickname = it.nickname, imageUrl = it.imageUrl) },
         inviteCode = inviteCode,
     )
-
-private fun convertMeetingDateTime(
-    date: String,
-    time: String,
-): MeetingDateTime {
-    val localDateTime = LocalDateTime.of(date.toLocalDate(), time.toLocalTime())
-    return MeetingDateTime(dateTime = localDateTime)
-}
-
-private fun String.toLocalDate(): LocalDate {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    return LocalDate.parse(this, formatter)
-}
-
-private fun String.toLocalTime(): LocalTime {
-    val formatter = DateTimeFormatter.ofPattern("HH:mm")
-    return LocalTime.parse(this, formatter)
-}
