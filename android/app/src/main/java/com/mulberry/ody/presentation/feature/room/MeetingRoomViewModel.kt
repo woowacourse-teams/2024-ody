f7ef21bc198baa4456f1bc377b76a71ed4c31fe3
kimhyemin <kimhm0728@naver.com>
2025-08-11 00:28:16 +0900

    chore: 불필요한 코드/의존성 제거

--- android/app/src/main/java/com/mulberry/ody/presentation/feature/room/MeetingRoomViewModel.kt
+++ android/app/src/main/java/com/mulberry/ody/presentation/feature/room/MeetingRoomViewModel.kt
@@ -95,9 +95,6 @@ class MeetingRoomViewModel
         private val _nudgeFailMate: MutableSharedFlow<Int> = MutableSharedFlow()
         val nudgeFailMate: SharedFlow<Int> get() = _nudgeFailMate.asSharedFlow()
 
-        private val _exitMeetingRoomEvent: MutableSharedFlow<Unit> = MutableSharedFlow()
-        val exitMeetingRoomEvent: SharedFlow<Unit> get() = _exitMeetingRoomEvent.asSharedFlow()
-
         private val matesNudgeTimes: MutableMap<Long, LocalDateTime> = mutableMapOf()
 
         private val _inaccessibleEtaEvent: MutableSharedFlow<Unit> = MutableSharedFlow()
@@ -276,7 +273,6 @@ class MeetingRoomViewModel
                 meetingRepository.exitMeeting(_meeting.value.id)
                     .onSuccess {
                         matesEtaRepository.closeEtaDashboard(meetingId)
-                        _exitMeetingRoomEvent.emit(Unit)
                     }.onFailure { code, errorMessage ->
                         handleError()
                         analyticsHelper.logNetworkErrorEvent(TAG, "$code $errorMessage")
