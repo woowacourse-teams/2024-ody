c545652330f330d73789316f2959c707ebfe472d
kimhyemin <kimhm0728@naver.com>
2025-10-03 22:57:56 +0900

    build: 의존성 버전 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -81,12 +81,11 @@ android {
 dependencies {
     // androidx
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.splashscreen)
-    implementation(libs.androidx.lifecycle.viewmodel)
+
+    // datastore
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.datastore.core.android)
 
