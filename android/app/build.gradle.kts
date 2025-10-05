d3a3282c273776641447a40d08fc46b55f1d77f0
kimhyemin <kimhm0728@naver.com>
2025-10-05 23:14:24 +0900

    build: compose preview 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -142,7 +142,8 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
 
     // hilt
     implementation(libs.hilt.android)
