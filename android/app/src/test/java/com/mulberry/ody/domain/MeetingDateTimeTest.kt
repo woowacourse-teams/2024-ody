d3bf244cda72ea10f0bb210c18b4cd486c4a028b
kimhyemin <kimhm0728@naver.com>
2025-10-05 23:27:46 +0900

    test: 실패하는 테스트 수정

--- android/app/src/test/java/com/mulberry/ody/domain/MeetingDateTimeTest.kt
+++ android/app/src/test/java/com/mulberry/ody/domain/MeetingDateTimeTest.kt
@@ -1,31 +1,44 @@
 package com.mulberry.ody.domain
 
 import com.mulberry.ody.domain.model.MeetingDateTime
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
-import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
 class MeetingDateTimeTest {
     @Test
     fun `약속 날짜와 시간이 현재보다 이후인 경우 유효하다`() {
-        assertDoesNotThrow {
-            MeetingDateTime(dateTime = LocalDateTime.now().plusDays(10))
-        }
+        // given
+        val datetime = MeetingDateTime(dateTime = LocalDateTime.now().plusDays(10))
+
+        // when
+        val actual = datetime.isValid()
+
+        // then
+        assertThat(actual).isEqualTo(true)
     }
 
     @Test
     fun `약속 날짜와 시간이 현재보다 이전인 경우 유효하지 않다`() {
-        assertThrows<IllegalArgumentException> {
-            MeetingDateTime(dateTime = LocalDateTime.now().minusDays(10))
-        }
+        // given
+        val datetime = MeetingDateTime(dateTime = LocalDateTime.now().minusDays(10))
+
+        // when
+        val actual = datetime.isValid()
+
+        // then
+        assertThat(actual).isEqualTo(false)
     }
 
     @Test
     fun `약속 날짜와 시간이 현재와 같은 경우 유효하지 않다`() {
-        assertThrows<IllegalArgumentException> {
-            MeetingDateTime(dateTime = LocalDateTime.now())
-        }
+        // given
+        val datetime = MeetingDateTime(dateTime = LocalDateTime.now())
+
+        // when
+        val actual = datetime.isValid()
+
+        // then
+        assertThat(actual).isEqualTo(false)
     }
 }
