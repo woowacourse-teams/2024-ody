e468d1eebcf5ddf21738903fbda36c3fa9926bff
kimhyemin <kimhm0728@naver.com>
2025-10-11 20:35:57 +0900

    refactor: 약속 생성 함수 usecase 분리

---
+++ android/app/src/main/java/com/mulberry/ody/domain/usecase/CreateMeetingUseCase.kt
@@ -0,0 +1,17 @@
+package com.mulberry.ody.domain.usecase
+
+import com.mulberry.ody.domain.apiresult.ApiResult
+import com.mulberry.ody.domain.model.MeetingCreationInfo
+import com.mulberry.ody.domain.repository.location.AddressRepository
+import com.mulberry.ody.domain.repository.ody.MeetingRepository
+import javax.inject.Inject
+
+class CreateMeetingUseCase
+@Inject
+constructor(
+    private val meetingRepository: MeetingRepository,
+) {
+    suspend operator fun invoke(meetingCreationInfo: MeetingCreationInfo): ApiResult<String> {
+        return meetingRepository.postMeeting(meetingCreationInfo)
+    }
+}
